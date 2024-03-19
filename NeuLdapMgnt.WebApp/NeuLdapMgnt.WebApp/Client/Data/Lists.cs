69c706578ed6cf7a8b88648722462dcbf88daa53
Szidor Patrik <patrikp987@gmail.com>
2024-03-19 21:01:02 +0100

    Created temp students''

---
+++ NeuLdapMgnt.WebApp/NeuLdapMgnt.WebApp/Client/Data/Lists.cs
@@ -0,0 +1,18 @@
+﻿using NeuLdapMgnt.WebApp.Client.Models;
+
+namespace NeuLdapMgnt.WebApp.Client.Data
+{
+	public static class Lists
+	{
+		public static List<StudentModel> Students { get; private set; } = new()
+		{
+			new (1, "Valamilyen", "Pistike", 11, "a"),
+			new (2, "Akármilyen", "Huculu", 12, "b"),
+			new (3, "Tóth", "Muculu", 13, "c"),
+			new (4, "Valamilyen", "Valaki", 11, "d"),
+			new (5, "Valaki", "Ismeretlen", 9, "a")
+		};
+
+		public static List<StudentModel> GetStudents() => Students;
+	}
+}
