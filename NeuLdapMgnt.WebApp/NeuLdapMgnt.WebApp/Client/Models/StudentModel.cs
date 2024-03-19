cc7ba0e3df50eca7595eb5f081e2252d91bac040
Szidor Patrik <patrikp987@gmail.com>
2024-03-19 21:03:19 +0100

    Created StudentModel.cs

---
+++ NeuLdapMgnt.WebApp/NeuLdapMgnt.WebApp/Client/Models/StudentModel.cs
@@ -0,0 +1,20 @@
+﻿namespace NeuLdapMgnt.WebApp.Client.Models
+{
+	public class StudentModel
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int ClassNumber { get; set; }
+		public string ClassSymbol { get; set; }
+
+		public StudentModel(int id, string firstName, string lastName, int classNumber, string classSymbol)
+		{
+			Id = id;
+			FirstName = firstName;
+			LastName = lastName;
+			ClassNumber = classNumber;
+			ClassSymbol = classSymbol;
+		}
+	}
+}
