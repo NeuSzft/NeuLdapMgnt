fcffcab3a06e221a7d07fb426287c5449889a450
Törőcsik Ádám <toarexer@gmail.com>
2024-03-21 11:04:45 +0100

    Added LdapHelper

---
+++ NeuLdapMgnt.Api/LdapHelper.cs
@@ -0,0 +1,72 @@
+using System;
+using System.DirectoryServices.Protocols;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Logging;
+using NeuLdapMgnt.Models;
+
+namespace NeuLdapMgnt.Api;
+
+public sealed class LdapHelper : IDisposable {
+    public readonly LdapConnection Connection;
+
+    public string   DnBase { get; init; } = string.Empty;
+    public ILogger? Logger { get; set; }
+
+    public LdapHelper(string server, int port, string user, string password) {
+        LdapDirectoryIdentifier identifier = new(server, port);
+        NetworkCredential       credential = new(user, password);
+
+        Connection = new(identifier, credential, AuthType.Basic) {
+            SessionOptions = {
+                ProtocolVersion = 3
+            }
+        };
+        Connection.Bind();
+    }
+
+    public void Dispose() => Connection.Dispose();
+
+    public DirectoryResponse? TryRequest(DirectoryRequest request) => TryRequest(request, out _);
+
+    public DirectoryResponse? TryRequest(DirectoryRequest request, out string? error) {
+        try {
+            error = null;
+            return Connection.SendRequest(request);
+        }
+        catch (DirectoryException e) {
+            error = e.Message;
+            Logger?.LogError(e.ToString());
+            return null;
+        }
+    }
+
+    public static readonly string AnyFilter = "(objectClass=*)";
+
+    public static T ParseEntry<T>(SearchResultEntry entry) where T : class {
+        Type             type = typeof(T);
+        ConstructorInfo? ctor = type.GetConstructor(Type.EmptyTypes);
+        T                obj  = ctor?.Invoke(null) as T ?? (T)RuntimeHelpers.GetUninitializedObject(type);
+
+        foreach (PropertyInfo info in type.GetProperties())
+            if (info.GetCustomAttribute<LdapAttributeAttribute>() is { } attribute) {
+                string? value = entry.Attributes[attribute.Name].GetValues(typeof(string)).FirstOrDefault() as string;
+                info.SetValue(obj, Convert.ChangeType(value, info.PropertyType));
+            }
+
+        return obj;
+    }
+
+    public static T? TryParseEntry<T>(SearchResultEntry entry, out string? error) where T : class {
+        try {
+            error = null;
+            return ParseEntry<T>(entry);
+        }
+        catch (Exception e) {
+            error = e.Message;
+            return null;
+        }
+    }
+}
--- NeuLdapMgnt.Api/Program.cs
+++ NeuLdapMgnt.Api/Program.cs
@@ -1,14 +1,12 @@
 ﻿using System;
+using System.DirectoryServices.Protocols;
 using System.Reflection;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
-using NeuLdapMgnt.Api.Endpoints;
 
 namespace NeuLdapMgnt.Api;
 
@@ -18,61 +16,20 @@ internal static class Program {
     public static readonly string TokenIssuer = Assembly.GetExecutingAssembly().FullName!;
 
     public static void Main(string[] args) {
-        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        LdapHelper ldapHelper = new("localhost", 389, "cn=admin,dc=test,dc=local", "admin") { DnBase = "dc=test,dc=local" };
+        ldapHelper.TryRequest(new AddRequest("ou=students,dc=test,dc=local", "organizationalUnit"));
+        ldapHelper.TryRequest(new AddRequest("ou=teachers,dc=test,dc=local", "organizationalUnit"));
 
-        // Create jwt authentication scheme
-        builder.Services.AddAuthentication("JwtBearer").AddJwtBearer("JwtBearer", options => {
-            options.TokenValidationParameters = new TokenValidationParameters {
-                ValidateAudience = false,
-                ValidateIssuer   = true,
-                ValidateLifetime = true,
-
-                IssuerSigningKey = SecurityKey,
-                ValidIssuers     = new[] { TokenIssuer },
-                ValidAlgorithms  = new[] { SecurityAlgorithms.HmacSha256 }
-            };
-
-            options.Events = new JwtBearerEvents {
-                // Try to set the jwt from cookie if Authorization header is not specified. Fail if neither are present.
-                OnMessageReceived = (MessageReceivedContext context) => {
-                    if (context.Request.Headers.ContainsKey("Authorization"))
-                        return Task.CompletedTask;
-
-                    if (context.Request.Cookies.TryGetValue("jwt", out var cookie))
-                        context.Token = cookie;
-                    else
-                        context.Fail("No json web token was specified");
-                    
-                    return Task.CompletedTask;
-                },
-
-                // Return 401 from middleware if authentication fails
-                OnChallenge = async (JwtBearerChallengeContext context) => {
-                    context.HandleResponse();
-                    if (context.AuthenticateFailure is not null) {
-                        context.Response.StatusCode  = StatusCodes.Status401Unauthorized;
-                        context.Response.ContentType = "text/plain";
-                        await context.Response.WriteAsync(context.ErrorDescription.DefaultIfNullOrEmpty("Invalid json web token"));
-                        await context.Response.CompleteAsync();
-                    }
-                },
-            };
-        });
-
-        // Add authorization policy that uses the previously created authentication scheme
-        builder.Services.AddAuthorization(options => {
-            options.AddPolicy("JwtBearerPolicy", policy => {
-                policy.RequireAuthenticatedUser();
-                policy.AuthenticationSchemes.Add("JwtBearer");
-            });
-        });
+        WebApplicationBuilder builder = WebApplication.CreateBuilder();
 
         // Add other services
         builder.Services.AddCors();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        builder.Services.AddSingleton<LdapHelper>(_ => ldapHelper);
 
         WebApplication app = builder.Build();
+        ldapHelper.Logger = app.Logger;
 
         // Add a middleware that logs each request to the console
         app.Use(async (HttpContext context, RequestDelegate next) => {
@@ -81,13 +38,9 @@ internal static class Program {
             app.Logger.LogInformation($"[{now:yyyy.MM.dd - HH:mm:ss}] {context.Request.Host} → {context.Request.Path} ({context.Response.StatusCode})");
         });
 
-        // Add middlewares for authentication
-        app.UseAuthentication();
-        app.UseAuthorization();
-
         // Set CORS to accept any connection
         app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
-        
+
         // Add swagger middlewares when in development mode
         if (app.Environment.IsDevelopment()) {
             app.UseSwagger();
@@ -95,7 +48,6 @@ internal static class Program {
         }
 
         // Map endpoints
-        app.MapAuthTestEndpoints();
 
         // Print the current security key as a base64 string
         app.Logger.LogCritical($"Key: {Convert.ToBase64String(SecurityKey.Key)}");
