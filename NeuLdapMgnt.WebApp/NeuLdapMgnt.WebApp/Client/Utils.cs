839e5d1104beb8dffdfca28caddd446d319e62d1
Szidor Patrik <patrikp987@gmail.com>
2024-03-22 01:43:53 +0100

    Implemented 'GetClone<T>(T? model)'

--- NeuLdapMgnt.WebApp/NeuLdapMgnt.WebApp/Client/Utils.cs
+++ NeuLdapMgnt.WebApp/NeuLdapMgnt.WebApp/Client/Utils.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Components;
+using NeuLdapMgnt.Models;
+using System.Text.Json;
 
 namespace NeuLdapMgnt.WebApp.Client
 {
 	public class Utils
 	{
+		public int Zero { get; set; } = 0;
+		public static T? GetClone<T>(T obj)
+		{
+			if (obj is null) return default;
 
+			string objJson = JsonSerializer.Serialize<T>(obj);
+			return JsonSerializer.Deserialize<T>(objJson);
+		}
 	}
 }
