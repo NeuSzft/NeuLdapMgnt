36e8769387bd081ebc5c8c14e44a8c6ce4487fdd
Szidor Patrik <patrikp987@gmail.com>
2024-03-30 16:51:33 +0100

    Refactoring

--- NeuLdapMgnt/WebApp/Client/Requests/StudentRequests.cs
+++ NeuLdapMgnt/WebApp/Client/Requests/StudentRequests.cs
@@ -6,30 +6,18 @@ namespace NeuLdapMgnt.WebApp.Client.Requests
 	{
 		// Sends a GET request to retrieve all student entries
 		public static async Task<RequestResult<Student>?> GetStudentsAsync(this ApiRequests apiRequests)
-		{
-			var result = await apiRequests.SendRequestAsync<Student>(HttpMethod.Get, "/students");
-			return result ?? null;
-		}
+			=> await apiRequests.SendRequestAsync<Student>(HttpMethod.Get, "/students");
 
 		// Sends a POST request to create a new student entry
 		public static async Task<RequestResult<Student>?> AddStudentAsync(this ApiRequests apiRequests, Student student)
-		{
-			var result = await apiRequests.SendRequestAsync<Student>(HttpMethod.Post, "/students", student);
-			return result ?? null;
-		}
+			=> await apiRequests.SendRequestAsync<Student>(HttpMethod.Post, "/students", student);
 
 		// Sends a PUT request to update the student by their ID
 		public static async Task<RequestResult<Student>?> UpdateStudentAsync(this ApiRequests apiRequests, long id, Student student)
-		{
-			var result = await apiRequests.SendRequestAsync<Student>(HttpMethod.Put, $"/students/{id}", student);
-			return result ?? null;
-		}
+			=> await apiRequests.SendRequestAsync<Student>(HttpMethod.Put, $"/students/{id}", student);
 
 		// Sends a DELETE request to delete the student by their ID
 		public static async Task<RequestResult<Student>?> DeleteStudentAsync(this ApiRequests apiRequests, long id)
-		{
-			var result = await apiRequests.SendRequestAsync<Student>(HttpMethod.Delete, $"/students/{id}");
-			return result ?? null;
-		}
+			=> await apiRequests.SendRequestAsync<Student>(HttpMethod.Delete, $"/students/{id}");
 	}
 }
