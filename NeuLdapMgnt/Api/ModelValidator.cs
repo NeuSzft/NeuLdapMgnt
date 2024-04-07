fb96efdc32e785c1f281833b7371acee8e2352bd
Törőcsik Ádám <toarexer@gmail.com>
2024-04-07 17:34:39 +0200

    Added doc comments to ModelValidator

--- NeuLdapMgnt/Api/ModelValidator.cs
+++ NeuLdapMgnt/Api/ModelValidator.cs
@@ -10,7 +10,12 @@ using NeuLdapMgnt.Models;
 
 namespace NeuLdapMgnt.Api;
 
+/// <summary>A helper for validating models with <see cref="ValidationAttribute"/>s.</summary>
 public static class ModelValidator {
+    /// <summary>Validates the <see cref="ValidationAttribute"/>s of a model.</summary>
+    /// <param name="obj">The object to validate.</param>
+    /// <typeparam name="T">The type of the model.</typeparam>
+    /// <returns>A <see cref="RequestResult{T}"/> containing the result of the validation.</returns>
     public static RequestResult<T> Validate<T>(T obj) where T : class {
         List<ValidationResult> results = new();
         if (Validator.TryValidateObject(obj, new ValidationContext(obj), results, true))
@@ -18,6 +23,10 @@ public static class ModelValidator {
         return new RequestResult<T>().SetStatus(StatusCodes.Status400BadRequest).SetErrors(results.Select(x => x.ErrorMessage).NotNull().ToArray());
     }
 
+    /// <summary>Tries to deserializes the json string, then validate the <see cref="ValidationAttribute"/>s of the model.</summary>
+    /// <param name="json">The json string to deserialize.</param>
+    /// <typeparam name="T">The type of the model.</typeparam>
+    /// <returns>A <see cref="RequestResult{T}"/> containing the result of the validation.</returns>
     public static RequestResult<T> ValidateJson<T>(string json) where T : class {
         T? obj;
         try {
@@ -30,6 +39,10 @@ public static class ModelValidator {
         return Validate(obj);
     }
 
+    /// <summary>Tries to deserializes the json body of the <see cref="HttpRequest"/>, then validate the <see cref="ValidationAttribute"/>s of the model.</summary>
+    /// <param name="request">The request containing the json to deserialize within it's body.</param>
+    /// <typeparam name="T">The type of the model.</typeparam>
+    /// <returns>A <see cref="RequestResult{T}"/> containing the result of the validation.</returns>
     public static async Task<RequestResult<T>> ValidateRequest<T>(HttpRequest request) where T : class {
         using StreamReader reader = new(request.Body);
         string             json   = await reader.ReadToEndAsync();
