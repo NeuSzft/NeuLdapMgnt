93d5d8429409d4e4ac4f585762f01003244c334f
Törőcsik Ádám <toarexer@gmail.com>
2024-04-09 22:19:16 +0200

    Fixed LdapService variable names

--- NeuLdapMgnt/Api/LdapServiceEntityExtensions.cs
+++ NeuLdapMgnt/Api/LdapServiceEntityExtensions.cs
@@ -10,25 +10,25 @@ namespace NeuLdapMgnt.Api;
 
 public static class LdapServiceEntityExtensions {
     /// <summary>Checks if the entity exists within the database using it's uid.</summary>
-    /// <param name="service">The <see cref="LdapService"/> the method should use.</param>
+    /// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
     /// <param name="id">The uid of the entity.</param>
     /// <typeparam name="T">The type of the entity.</typeparam>
     /// <returns>Returns <c>true</c> if the entity exists. If it does not exist or the search request fails it returns <c>false</c>.</returns>
-    public static bool EntityExists<T>(this LdapService service, long id) where T : class {
-        SearchRequest   request  = new($"uid={id},ou={typeof(T).GetOuName()},{service.DnBase}", LdapService.AnyFilter, SearchScope.Base, null);
-        SearchResponse? response = service.TryRequest(request) as SearchResponse;
+    public static bool EntityExists<T>(this LdapService ldap, long id) where T : class {
+        SearchRequest   request  = new($"uid={id},ou={typeof(T).GetOuName()},{ldap.DnBase}", LdapService.AnyFilter, SearchScope.Base, null);
+        SearchResponse? response = ldap.TryRequest(request) as SearchResponse;
 
         return response?.Entries.Count == 1;
     }
 
     /// <summary>Gets all entities of the specified type from the database.</summary>
-    /// <param name="service">The <see cref="LdapService"/> the method should use.</param>
+    /// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
     /// <typeparam name="T">The type of the entity.</typeparam>
     /// <returns>An <see cref="IEnumerable{T}"/> containing the entities.</returns>
     /// <remarks>If an entity cannot be parsed then it is ignored and not returned.</remarks>
-    public static IEnumerable<T> GetAllEntities<T>(this LdapService service) where T : class, new() {
-        SearchRequest   request  = new($"ou={typeof(T).GetOuName()},{service.DnBase}", LdapService.AnyFilter, SearchScope.Subtree, null);
-        SearchResponse? response = service.TryRequest(request) as SearchResponse;
+    public static IEnumerable<T> GetAllEntities<T>(this LdapService ldap) where T : class, new() {
+        SearchRequest   request  = new($"ou={typeof(T).GetOuName()},{ldap.DnBase}", LdapService.AnyFilter, SearchScope.Subtree, null);
+        SearchResponse? response = ldap.TryRequest(request) as SearchResponse;
 
         if (response is null)
             yield break;
@@ -39,13 +39,13 @@ public static class LdapServiceEntityExtensions {
     }
 
     /// <summary>Tries to get the entity from the database using it's uid.</summary>
-    /// <param name="service">The <see cref="LdapService"/> the method should use.</param>
+    /// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
     /// <param name="id">The uid of the entity.</param>
     /// <typeparam name="T">The type of the entity.</typeparam>
     /// <returns>A <see cref="RequestResult{T}"/> containing the outcome of the operation.</returns>
-    public static RequestResult<T> TryGetEntity<T>(this LdapService service, long id) where T : class, new() {
-        SearchRequest   request  = new($"uid={id},ou={typeof(T).GetOuName()},{service.DnBase}", LdapService.AnyFilter, SearchScope.Base, null);
-        SearchResponse? response = service.TryRequest(request, out var error) as SearchResponse;
+    public static RequestResult<T> TryGetEntity<T>(this LdapService ldap, long id) where T : class, new() {
+        SearchRequest   request  = new($"uid={id},ou={typeof(T).GetOuName()},{ldap.DnBase}", LdapService.AnyFilter, SearchScope.Base, null);
+        SearchResponse? response = ldap.TryRequest(request, out var error) as SearchResponse;
 
         if (response is null || response.Entries.Count == 0)
             return new RequestResult<T>().SetStatus(StatusCodes.Status404NotFound).SetErrors(error ?? "The object does not exist.");
@@ -57,44 +57,44 @@ public static class LdapServiceEntityExtensions {
     }
 
     /// <summary>Tries to add the entity to the database with the specified uid.</summary>
-    /// <param name="service">The <see cref="LdapService"/> the method should use.</param>
+    /// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
     /// <param name="id">The uid of the entity.</param>
     /// <param name="entity">The entity to be added.</param>
     /// <typeparam name="T">The type of the entity.</typeparam>
     /// <returns>A <see cref="RequestResult{T}"/> containing the outcome of the operation.</returns>
-    public static RequestResult<T> TryAddEntity<T>(this LdapService service, T entity, long id) where T : class {
+    public static RequestResult<T> TryAddEntity<T>(this LdapService ldap, T entity, long id) where T : class {
         Type type = typeof(T);
 
-        service.TryRequest(new AddRequest($"ou={type.GetOuName()},{service.DnBase}", "organizationalUnit"));
+        ldap.TryRequest(new AddRequest($"ou={type.GetOuName()},{ldap.DnBase}", "organizationalUnit"));
 
-        if (service.EntityExists<T>(id))
+        if (ldap.EntityExists<T>(id))
             return new RequestResult<T>().SetStatus(StatusCodes.Status409Conflict).SetErrors("The object already exists.");
 
-        AddRequest request = new($"uid={id},ou={type.GetOuName()},{service.DnBase}");
+        AddRequest request = new($"uid={id},ou={type.GetOuName()},{ldap.DnBase}");
         if (type.GetCustomAttribute<LdapObjectClassesAttribute>() is { } objectClasses)
             request.Attributes.Add(new("objectClass", objectClasses.Classes.Cast<object>().ToArray()));
 
         foreach (DirectoryAttribute attribute in LdapService.GetDirectoryAttributes(entity))
             request.Attributes.Add(attribute);
 
-        if (service.TryRequest(request, out var error) is not null)
+        if (ldap.TryRequest(request, out var error) is not null)
             return new RequestResult<T>().SetStatus(StatusCodes.Status201Created).SetValues(entity);
         return new RequestResult<T>().SetStatus(StatusCodes.Status400BadRequest).SetErrors(error ?? string.Empty);
     }
 
     /// <summary>Tries to modify an entity in the database with the specified uid.</summary>
-    /// <param name="service">The <see cref="LdapService"/> the method should use.</param>
+    /// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
     /// <param name="id">The uid of the entity to replace.</param>
     /// <param name="entity">The entity that will replace the current one.</param>
     /// <typeparam name="T">The type of the entity.</typeparam>
     /// <returns>A <see cref="RequestResult{T}"/> containing the outcome of the operation.</returns>
-    public static RequestResult<T> TryModifyEntity<T>(this LdapService service, T entity, long id) where T : class {
+    public static RequestResult<T> TryModifyEntity<T>(this LdapService ldap, T entity, long id) where T : class {
         Type type = typeof(T);
 
-        if (!service.EntityExists<T>(id))
+        if (!ldap.EntityExists<T>(id))
             return new RequestResult<T>().SetStatus(StatusCodes.Status404NotFound).SetErrors("The object does not exist.");
 
-        ModifyRequest request = new($"uid={id},ou={typeof(T).GetOuName()},{service.DnBase}");
+        ModifyRequest request = new($"uid={id},ou={typeof(T).GetOuName()},{ldap.DnBase}");
 
         foreach (PropertyInfo info in type.GetProperties())
             if (info.GetCustomAttribute(typeof(LdapAttributeAttribute)) is LdapAttributeAttribute attribute) {
@@ -106,37 +106,37 @@ public static class LdapServiceEntityExtensions {
                 request.Modifications.Add(mod);
             }
 
-        if (service.TryRequest(request, out var error) is not null)
+        if (ldap.TryRequest(request, out var error) is not null)
             return new RequestResult<T>().SetStatus(StatusCodes.Status200OK).SetValues(entity);
         return new RequestResult<T>().SetStatus(StatusCodes.Status400BadRequest).SetErrors(error ?? string.Empty);
     }
 
     /// <summary>Tries to remove an entity from the database with the specified uid.</summary>
-    /// <param name="service">The <see cref="LdapService"/> the method should use.</param>
+    /// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
     /// <param name="id">The uid of the entity to remove.</param>
     /// <typeparam name="T">The type of the entity.</typeparam>
     /// <returns>A <see cref="RequestResult"/> containing the outcome of the operation.</returns>
-    public static RequestResult TryDeleteEntity<T>(this LdapService service, long id) where T : class {
-        if (!service.EntityExists<T>(id))
+    public static RequestResult TryDeleteEntity<T>(this LdapService ldap, long id) where T : class {
+        if (!ldap.EntityExists<T>(id))
             return new RequestResult().SetStatus(StatusCodes.Status404NotFound).SetErrors("The object does not exist.");
 
-        DeleteRequest request = new($"uid={id},ou={typeof(T).GetOuName()},{service.DnBase}");
+        DeleteRequest request = new($"uid={id},ou={typeof(T).GetOuName()},{ldap.DnBase}");
 
-        if (service.TryRequest(request, out var error) is not null)
+        if (ldap.TryRequest(request, out var error) is not null)
             return new RequestResult().SetStatus(StatusCodes.Status200OK);
         return new RequestResult().SetStatus(StatusCodes.Status400BadRequest).SetErrors(error ?? string.Empty);
     }
 
     /// <summary>Tries to add the entities to the database.</summary>
-    /// <param name="service">The <see cref="LdapService"/> the method should use.</param>
+    /// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
     /// <param name="entities">The entities to be added.</param>
     /// <param name="idGetter">A delegate that takes in an entity of type <typeparamref name="T"/> and returns an uid with the type of <c>long</c>.</param>
     /// <typeparam name="T">The type of the entities.</typeparam>
     /// <returns>A <see cref="RequestResult"/> containing the outcome of the operation.</returns>
-    public static RequestResult TryAddEntities<T>(this LdapService service, IEnumerable<T> entities, Func<T, long> idGetter) where T : class {
+    public static RequestResult TryAddEntities<T>(this LdapService ldap, IEnumerable<T> entities, Func<T, long> idGetter) where T : class {
         Type type = typeof(T);
 
-        service.TryRequest(new AddRequest($"ou={type.GetOuName()},{service.DnBase}", "organizationalUnit"));
+        ldap.TryRequest(new AddRequest($"ou={type.GetOuName()},{ldap.DnBase}", "organizationalUnit"));
 
         DirectoryAttribute? objectClassesAttribute = null;
         if (type.GetCustomAttribute<LdapObjectClassesAttribute>() is { } objectClasses)
@@ -145,7 +145,7 @@ public static class LdapServiceEntityExtensions {
         var requests = entities.Select(x => {
             long id = idGetter(x);
 
-            AddRequest request = new($"uid={id},ou={type.GetOuName()},{service.DnBase}");
+            AddRequest request = new($"uid={id},ou={type.GetOuName()},{ldap.DnBase}");
             if (objectClassesAttribute is not null)
                 request.Attributes.Add(objectClassesAttribute);
 
@@ -155,7 +155,7 @@ public static class LdapServiceEntityExtensions {
             return new UniqueDirectoryRequest(request, id.ToString());
         });
 
-        string[] errors = service.TryRequests(requests).Select(x => x.Error).NotNull().ToArray();
+        string[] errors = ldap.TryRequests(requests).Select(x => x.Error).NotNull().ToArray();
         return new RequestResult().SetStatus(StatusCodes.Status207MultiStatus).SetErrors(errors);
     }
 }
--- NeuLdapMgnt/Api/LdapServiceGroupExtensions.cs
+++ NeuLdapMgnt/Api/LdapServiceGroupExtensions.cs
@@ -19,14 +19,14 @@ public static class LdapServiceGroupExtensions {
     }
 
     /// <summary>Checks if the entity with the uid is part of the group.</summary>
-    /// <param name="service">The <see cref="LdapService"/> the method should use.</param>
+    /// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
     /// <param name="name">The name of the group.</param>
     /// <param name="id">The uid of the entity.</param>
     /// <returns><c>true</c> if the entity is part of the group, <c>false</c> if not or the group does not exist.</returns>
     /// <remarks>This method does not check if the entity actually exists or not.</remarks>
-    public static bool PartOfGroup(this LdapService service, string name, long id) {
-        SearchRequest   request  = new($"ou={name},{service.DnBase}", LdapService.AnyFilter, SearchScope.Base, null);
-        SearchResponse? response = service.TryRequest(request) as SearchResponse;
+    public static bool PartOfGroup(this LdapService ldap, string name, long id) {
+        SearchRequest   request  = new($"ou={name},{ldap.DnBase}", LdapService.AnyFilter, SearchScope.Base, null);
+        SearchResponse? response = ldap.TryRequest(request) as SearchResponse;
 
         if (response is null || response.Entries.Count == 0)
             return false;
@@ -38,50 +38,50 @@ public static class LdapServiceGroupExtensions {
     }
 
     /// <summary>Tries to add the group to the database with the specified name.</summary>
-    /// <param name="service">The <see cref="LdapService"/> the method should use.</param>
+    /// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
     /// <param name="name">The name of the group.</param>
     /// <returns>A <see cref="RequestResult"/> containing the outcome of the operation.</returns>
-    public static RequestResult TryAddGroup(this LdapService service, string name) {
-        if (service.GroupExists(name))
+    public static RequestResult TryAddGroup(this LdapService ldap, string name) {
+        if (ldap.GroupExists(name))
             return new RequestResult().SetStatus(StatusCodes.Status409Conflict).SetErrors("The group already exists.");
 
-        AddRequest request = new($"ou={name},{service.DnBase}",
+        AddRequest request = new($"ou={name},{ldap.DnBase}",
             new("objectClass", "organizationalUnit", "uidObject"),
             new("uid", "__DEFAULT__")
         );
 
-        if (service.TryRequest(request, out var error) is not null)
+        if (ldap.TryRequest(request, out var error) is not null)
             return new RequestResult().SetStatus(StatusCodes.Status201Created);
         return new RequestResult().SetStatus(StatusCodes.Status400BadRequest).SetErrors(error ?? string.Empty);
     }
 
     /// <summary>Tries to remove an entity from the database with the specified uid.</summary>
-    /// <param name="service">The <see cref="LdapService"/> the method should use.</param>
+    /// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
     /// <param name="name">The name of the group.</param>
     /// <returns>A <see cref="RequestResult"/> containing the outcome of the operation.</returns>
-    public static RequestResult TryDeleteGroup(this LdapService service, string name) {
-        if (!service.GroupExists(name))
+    public static RequestResult TryDeleteGroup(this LdapService ldap, string name) {
+        if (!ldap.GroupExists(name))
             return new RequestResult().SetStatus(StatusCodes.Status404NotFound).SetErrors("The group does not exist.");
 
-        DeleteRequest request = new($"ou={name},{service.DnBase}");
+        DeleteRequest request = new($"ou={name},{ldap.DnBase}");
 
-        if (service.TryRequest(request, out var error) is not null)
+        if (ldap.TryRequest(request, out var error) is not null)
             return new RequestResult().SetStatus(StatusCodes.Status200OK);
         return new RequestResult().SetStatus(StatusCodes.Status400BadRequest).SetErrors(error ?? string.Empty);
     }
 
     /// <summary>Tries to add the entity to the group.</summary>
-    /// <param name="service">The <see cref="LdapService"/> the method should use.</param>
+    /// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
     /// <param name="name">The name of the group.</param>
     /// <param name="id">The uid of the entity to add.</param>
     /// <returns>A <see cref="RequestResult"/> containing the outcome of the operation.</returns>
-    public static RequestResult TryAddEntityToGroup(this LdapService service, string name, long id) {
-        service.TryAddGroup(name);
+    public static RequestResult TryAddEntityToGroup(this LdapService ldap, string name, long id) {
+        ldap.TryAddGroup(name);
 
-        if (service.PartOfGroup(name, id))
+        if (ldap.PartOfGroup(name, id))
             return new RequestResult().SetStatus(StatusCodes.Status409Conflict).SetErrors("Already part of group.");
 
-        ModifyRequest request = new($"ou={name},{service.DnBase}");
+        ModifyRequest request = new($"ou={name},{ldap.DnBase}");
 
         DirectoryAttributeModification mod = new() {
             Name      = "uid",
@@ -90,21 +90,21 @@ public static class LdapServiceGroupExtensions {
         mod.Add(id.ToString());
         request.Modifications.Add(mod);
 
-        if (service.TryRequest(request, out var error) is not null)
+        if (ldap.TryRequest(request, out var error) is not null)
             return new RequestResult().SetStatus(StatusCodes.Status200OK);
         return new RequestResult().SetStatus(StatusCodes.Status400BadRequest).SetErrors(error ?? string.Empty);
     }
 
     /// <summary>Tries to add the entities to the group.</summary>
-    /// <param name="service">The <see cref="LdapService"/> the method should use.</param>
+    /// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
     /// <param name="name">The name of the group.</param>
     /// <param name="ids">The uids to be added.</param>
     /// <returns>A <see cref="RequestResult"/> containing the outcome of the operation.</returns>
-    public static RequestResult TryAddEntitiesToGroup(this LdapService service, string name, IEnumerable<long> ids) {
-        service.TryAddGroup(name);
+    public static RequestResult TryAddEntitiesToGroup(this LdapService ldap, string name, IEnumerable<long> ids) {
+        ldap.TryAddGroup(name);
 
         var requests = ids.Select(id => {
-            ModifyRequest request = new($"ou={name},{service.DnBase}");
+            ModifyRequest request = new($"ou={name},{ldap.DnBase}");
 
             DirectoryAttributeModification mod = new() {
                 Name      = "uid",
@@ -116,23 +116,23 @@ public static class LdapServiceGroupExtensions {
             return new UniqueDirectoryRequest(request, id.ToString());
         });
 
-        string[] errors = service.TryRequests(requests).Select(x => x.Error).NotNull().ToArray();
+        string[] errors = ldap.TryRequests(requests).Select(x => x.Error).NotNull().ToArray();
         return new RequestResult().SetStatus(StatusCodes.Status207MultiStatus).SetErrors(errors);
     }
 
     /// <summary>Tries to remove the entity from the group.</summary>
-    /// <param name="service">The <see cref="LdapService"/> the method should use.</param>
+    /// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
     /// <param name="name">The name of the group.</param>
     /// <param name="id">The uid of the entity to remove.</param>
     /// <returns>A <see cref="RequestResult"/> containing the outcome of the operation.</returns>
-    public static RequestResult TryRemoveEntityFromGroup(this LdapService service, string name, long id) {
-        if (!service.GroupExists(name))
+    public static RequestResult TryRemoveEntityFromGroup(this LdapService ldap, string name, long id) {
+        if (!ldap.GroupExists(name))
             return new RequestResult().SetStatus(StatusCodes.Status404NotFound).SetErrors("The group does not exist.");
 
-        if (!service.PartOfGroup(name, id))
+        if (!ldap.PartOfGroup(name, id))
             return new RequestResult().SetStatus(StatusCodes.Status404NotFound).SetErrors("Not part of group.");
 
-        ModifyRequest request = new($"ou={name},{service.DnBase}");
+        ModifyRequest request = new($"ou={name},{ldap.DnBase}");
 
         DirectoryAttributeModification mod = new() {
             Name      = "uid",
@@ -141,22 +141,22 @@ public static class LdapServiceGroupExtensions {
         mod.Add(id.ToString());
         request.Modifications.Add(mod);
 
-        if (service.TryRequest(request, out var error) is not null)
+        if (ldap.TryRequest(request, out var error) is not null)
             return new RequestResult().SetStatus(StatusCodes.Status200OK);
         return new RequestResult().SetStatus(StatusCodes.Status400BadRequest).SetErrors(error ?? string.Empty);
     }
 
     /// <summary>Tries to remove the entities from the group.</summary>
-    /// <param name="service">The <see cref="LdapService"/> the method should use.</param>
+    /// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
     /// <param name="name">The name of the group.</param>
     /// <param name="ids">The uids to be removed.</param>
     /// <returns>A <see cref="RequestResult"/> containing the outcome of the operation.</returns>
-    public static RequestResult TryRemoveEntitiesFromGroup(this LdapService service, string name, IEnumerable<long> ids) {
-        if (!service.GroupExists(name))
+    public static RequestResult TryRemoveEntitiesFromGroup(this LdapService ldap, string name, IEnumerable<long> ids) {
+        if (!ldap.GroupExists(name))
             return new RequestResult().SetStatus(StatusCodes.Status404NotFound).SetErrors("The group does not exist.");
 
         var requests = ids.Select(id => {
-            ModifyRequest request = new($"ou={name},{service.DnBase}");
+            ModifyRequest request = new($"ou={name},{ldap.DnBase}");
 
             DirectoryAttributeModification mod = new() {
                 Name      = "uid",
@@ -168,7 +168,7 @@ public static class LdapServiceGroupExtensions {
             return new UniqueDirectoryRequest(request, id.ToString());
         });
 
-        string[] errors = service.TryRequests(requests).Select(x => x.Error).NotNull().ToArray();
+        string[] errors = ldap.TryRequests(requests).Select(x => x.Error).NotNull().ToArray();
         return new RequestResult().SetStatus(StatusCodes.Status207MultiStatus).SetErrors(errors);
     }
 }
