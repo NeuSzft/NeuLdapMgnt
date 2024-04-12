7d2bd5b9516b45734df4390569370297513873fb
Törőcsik Ádám <toarexer@gmail.com>
2024-04-12 10:46:19 +0200

    Fixed Teacher model

--- NeuLdapMgnt/Models/Teacher.cs
+++ NeuLdapMgnt/Models/Teacher.cs
@@ -1,21 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
-namespace NeuLdapMgnt.Models
-{
-	public class Teacher : Person
-	{
-        [Required, RegularExpression("^7[0-9]{10}$")]
-        [JsonPropertyName("id")]
-        [LdapAttribute("uid")]
-        public string Id { get; set; }
+namespace NeuLdapMgnt.Models;
 
-		[Required, Range(4000, 5999)]
-		[LdapAttribute("uid")]
-		public override int Uid { get; set; } = 4000;
+public class Teacher : Person {
+    [RegularExpression(@"^.+\..+$")]
+    [JsonPropertyName("id")]
+    [LdapAttribute("uid")]
+    public string Id { get; set; }
 
-		[Required, Range(4000, 5999)]
-		[LdapAttribute("gidNumber")]
-		public override int Gid { get; set; } = 4000;
-	}
+    [Required, Range(4001, 5999)]
+    [JsonRequired, JsonPropertyName("uid")]
+    [LdapAttribute("uidNumber")]
+    public override int Uid { get; set; } = 4001;
+
+    [Required, Range(4001, 5999)]
+    [JsonRequired, JsonPropertyName("gid")]
+    [LdapAttribute("gidNumber")]
+    public override int Gid { get; set; } = 4001;
 }
