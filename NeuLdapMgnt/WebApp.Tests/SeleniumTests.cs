fed366c5ebcb6473122285b5d3d5d3fe4ad2f713
Szidor Patrik <patrikp987@gmail.com>
2024-04-14 15:35:28 +0200

    Added asserts to test redirections

--- NeuLdapMgnt/WebApp.Tests/SeleniumTests.cs
+++ NeuLdapMgnt/WebApp.Tests/SeleniumTests.cs
@@ -13,18 +13,17 @@ namespace NeuLdapMgnt.WebApp.Tests
 	[TestClass]
 	public class SeleniumTests
 	{
-		private const string SutHub = "http://selenium-hub:4444";
-		private const string SutAirlines = "http://nginx-selenium:80";
+		private const string SutHub = "http://selenium-hub:4444/";
+		private const string SutMngt = "http://nginx-selenium:80/";
 		private IWebDriver _webDriver = null!;
 		private WebDriverWait _wait = null!;
-		private readonly HttpClient _httpClient = new();
 
 		[TestInitialize]
 		public void InitializeTest()
 		{
 			var firefoxOptions = new FirefoxOptions();
 			_webDriver = new RemoteWebDriver(new Uri(SutHub), firefoxOptions.ToCapabilities());
-			_webDriver.Navigate().GoToUrl(SutAirlines);
+			_webDriver.Navigate().GoToUrl(SutMngt);
 			_wait = new(_webDriver, TimeSpan.FromMilliseconds(3000));
 		}
 
@@ -37,6 +36,30 @@ namespace NeuLdapMgnt.WebApp.Tests
 		[TestMethod]
 		public void RedirectionIsWorkingWhenUnauthorized()
 		{
+			_wait.Until(ExpectedConditions.UrlContains("login"));
+			Assert.IsTrue(_webDriver.Url.Contains("login"));
+			
+			_webDriver.Navigate().GoToUrl(SutMngt + "students");
+			_wait.Until(ExpectedConditions.UrlContains("/login"));
+			Assert.IsTrue(_webDriver.Url.Contains("/login"));
+			
+			_webDriver.Navigate().GoToUrl(SutMngt + "students/add");
+			_wait.Until(ExpectedConditions.UrlContains("/login"));
+			Assert.IsTrue(_webDriver.Url.Contains("/login"));
+			
+			_webDriver.Navigate().GoToUrl(SutMngt + "teachers");
+			_wait.Until(ExpectedConditions.UrlContains("/login"));
+			Assert.IsTrue(_webDriver.Url.Contains("/login"));
+			
+			_webDriver.Navigate().GoToUrl(SutMngt + "teachers/add");
+			_wait.Until(ExpectedConditions.UrlContains("/login"));
+			Assert.IsTrue(_webDriver.Url.Contains("/login"));
+			
+			_webDriver.Navigate().GoToUrl(SutMngt + "admins");
+			_wait.Until(ExpectedConditions.UrlContains("/login"));
+			Assert.IsTrue(_webDriver.Url.Contains("/login"));
+			
+			_webDriver.Navigate().GoToUrl(SutMngt + "admins/add");
 			_wait.Until(ExpectedConditions.UrlContains("/login"));
 			Assert.IsTrue(_webDriver.Url.Contains("/login"));
 		}
