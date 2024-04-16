858c1daa7fd9c10b86d14e6dbb538aef473749b7
Szidor Patrik <patrikp987@gmail.com>
2024-04-16 18:16:34 +0200

    Create DirectoryAttribute.cs

---
+++ NeuLdapMgnt/Models/CustomValidationAttributes/DirectoryAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NeuLdapMgnt.Models.CustomValidationAttributes
+{
+	public class DirectoryAttribute : ValidationAttribute
+	{
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			if (value is string directory)
+			{
+				directory = directory.Trim();
+				if (!directory.StartsWith("/home/", StringComparison.OrdinalIgnoreCase))
+				{
+					return new ValidationResult("Directory must start with '/home/'.",
+						new[] { validationContext.MemberName }!);
+				}
+				else if (directory.StartsWith("/home/", StringComparison.OrdinalIgnoreCase)
+					&& (directory.EndsWith("/home/", StringComparison.OrdinalIgnoreCase)
+					|| directory.EndsWith("/", StringComparison.OrdinalIgnoreCase)))
+				{
+					return new ValidationResult("Directory must not end with '/'.",
+						new[] { validationContext.MemberName }!);
+				}
+				else
+				{
+					return ValidationResult.Success;
+				}
+			}
+
+			return new ValidationResult("Class: Invalid data type", new[] { validationContext.MemberName }!);
+		}
+	}
+}
