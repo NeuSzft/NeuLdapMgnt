b43feb387001d7c97b5da6ac5011c12f3f6be39f
Törőcsik Ádám <toarexer@gmail.com>
2024-04-17 21:49:04 +0200

    Fixed and documented LdapServiceDbDumpExtensions

--- NeuLdapMgnt/Api/LdapServiceDbDumpExtensions.cs
+++ NeuLdapMgnt/Api/LdapServiceDbDumpExtensions.cs
@@ -4,49 +4,59 @@ using NeuLdapMgnt.Models;
 
 namespace NeuLdapMgnt.Api;
 
-// TODO: Add xml doc comments to this class
 public static class LdapServiceDbDumpExtensions {
-    private static RequestResult EraseDatabase(this LdapService ldap) {
-        List<string> errors = ldap.EraseTreeElements(ldap.DomainComponents);
-        return new RequestResult().SetStatus(errors.Count == 0 ? StatusCodes.Status200OK : StatusCodes.Status500InternalServerError).SetErrors(errors.ToArray());
-    }
+	/// <summary>Erases the entire LDAP database.</summary>
+	/// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
+	/// <returns>A <see cref="RequestResult"/> containing the outcome of the operation.</returns>
+	private static RequestResult EraseDatabase(this LdapService ldap) {
+		List<string> errors = ldap.EraseTreeElements(ldap.DomainComponents);
+		return new RequestResult().SetStatus(errors.Count == 0 ? StatusCodes.Status200OK : StatusCodes.Status500InternalServerError).SetErrors(errors.ToArray());
+	}
 
-    public static RequestResult ImportDatabase(this LdapService ldap, LdapDbDump dump, bool erase = false) {
-        if (erase) {
-            RequestResult result = ldap.EraseDatabase();
-            if (result.Errors.Length > 0)
-                return result;
-        }
+	/// <summary>Imports a previous dump into the LDAP database.</summary>
+	/// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
+	/// <param name="dump">The <see cref="LdapDbDump"/> to import.</param>
+	/// <param name="erase">Determines whether the database should be erased before importing.</param>
+	/// <returns>A <see cref="RequestResult"/> containing the outcome of the operation.</returns>
+	public static RequestResult ImportDatabase(this LdapService ldap, LdapDbDump dump, bool erase = false) {
+		if (erase) {
+			RequestResult result = ldap.EraseDatabase();
+			if (result.Errors.Length > 0)
+				return result;
+		}
 
-        List<string> errors = new();
+		List<string> errors = new();
 
-        errors.AddRange(ldap.TryAddEntities(dump.Students, student => student.Id.ToString()).Errors);
-        errors.AddRange(ldap.TryAddEntities(dump.Teachers, teacher => teacher.Id).Errors);
-        errors.AddRange(ldap.TryAddEntitiesToGroup("inactive", dump.Inactives).Errors);
-        errors.AddRange(ldap.TryAddEntitiesToGroup("admin", dump.Admins).Errors);
+		errors.AddRange(ldap.TryAddEntities(dump.Students, student => student.Id.ToString()).Errors);
+		errors.AddRange(ldap.TryAddEntities(dump.Teachers, teacher => teacher.Id).Errors);
+		errors.AddRange(ldap.TryAddEntitiesToGroup("inactive", dump.Inactives).Errors);
+		errors.AddRange(ldap.TryAddEntitiesToGroup("admin", dump.Admins).Errors);
 
-        foreach (var item in dump.Values) {
-            ldap.SetValue(item.Key, item.Value, out var error);
-            if (error is not null)
-                errors.Add(error);
-        }
+		foreach (var item in dump.Values) {
+			ldap.SetValue(item.Key, item.Value, out var error);
+			if (error is not null)
+				errors.Add(error);
+		}
 
-        return new RequestResult().SetStatus(StatusCodes.Status207MultiStatus).SetErrors(errors.ToArray());
-    }
+		return new RequestResult().SetStatus(StatusCodes.Status207MultiStatus).SetErrors(errors.ToArray());
+	}
 
-    public static RequestResult<LdapDbDump> ExportDatabase(this LdapService ldap) {
-        LdapDbDump dump = new() {
-            Students  = ldap.GetAllEntities<Student>(),
-            Teachers  = ldap.GetAllEntities<Teacher>(),
-            Inactives = ldap.GetMembersOfGroup("inactive"),
-            Admins    = ldap.GetMembersOfGroup("admin"),
-            Values    = ldap.GetAllValues(out var error)
-        };
+	/// <summary>Exports the LDAP database into a <see cref="LdapDbDump"/>.</summary>
+	/// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
+	/// <returns>A <see cref="RequestResult{T}">RequestResult&lt;LdapDbDump&gt;</see> containing the outcome of the operation.</returns>
+	public static RequestResult<LdapDbDump> ExportDatabase(this LdapService ldap) {
+		LdapDbDump dump = new() {
+			Students = ldap.GetAllEntities<Student>(true),
+			Teachers = ldap.GetAllEntities<Teacher>(true),
+			Inactives = ldap.GetMembersOfGroup("inactive"),
+			Admins = ldap.GetMembersOfGroup("admin"),
+			Values = ldap.GetAllValues(out var error)
+		};
 
-        var response = new RequestResult<LdapDbDump>().SetStatus(StatusCodes.Status207MultiStatus).SetValues(dump);
-        if (error is not null)
-            response.SetErrors(error);
+		var response = new RequestResult<LdapDbDump>().SetStatus(StatusCodes.Status207MultiStatus).SetValues(dump);
+		if (error is not null)
+			response.SetErrors(error);
 
-        return response;
-    }
+		return response;
+	}
 }
