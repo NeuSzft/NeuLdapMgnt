ec43f1a86ce4699ad2403ed65fa184e44231bf09
Szidor Patrik <patrikp987@gmail.com>
2024-04-18 12:45:10 +0200

    Created PostgresService.cs

---
+++ NeuLdapMgnt/Api/PostgresService.cs
@@ -0,0 +1,125 @@
+﻿using Dapper;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using NeuLdapMgnt.Models;
+using System.Net;
+
+namespace NeuLdapMgnt.Api
+{
+	public sealed class PostgresService
+	{
+		private readonly NpgsqlConnection _connection;
+
+		public PostgresService(string host, string db, string password)
+		{
+			_connection = new($"Host={host};Database={db};Username=postgres;Password={password}");
+			CreateDefaultTables();
+		}
+
+		public static PostgresService FromEnvs(string hostEnv = "POSTGRES_HOST", string dbEnv = "POSTGRES_DB", string passwordEnv = "POSTGRES_PASSWORD")
+		{
+			return new(
+				Environment.GetEnvironmentVariable(hostEnv).ThrowIfNullOrEmpty(),
+				Environment.GetEnvironmentVariable(dbEnv).ThrowIfNullOrEmpty(),
+				Environment.GetEnvironmentVariable(passwordEnv).ThrowIfNullOrEmpty()
+			);
+		}
+
+		public void CreateLogEntry(LogEntry entry)
+		{
+			string query;
+
+			if (string.IsNullOrEmpty(entry.Username) || string.IsNullOrEmpty(entry.FullName))
+			{
+				query = """
+					INSERT INTO "entries"
+					("time", "log_level", "username", "host", "method", "request_path", "status_code")
+					VALUES
+					(@Time, @LogLevel, @Username, @Host, @Method, @RequestPath, @StatusCode);
+				""";
+			}
+			else
+			{
+				query = """
+					INSERT INTO "users"
+					("username", "full_name")
+					VALUES
+					(@Username, @FullName)
+					ON CONFLICT DO NOTHING;
+					
+					INSERT INTO "entries"
+					("time", "log_level", "username", "host", "method", "request_path", "status_code")
+					VALUES
+					(@Time, @LogLevel, @Username, @Host, @Method, @RequestPath, @StatusCode);
+				""";
+			}
+
+			_connection.Open();
+			_connection.Execute(query, new
+			{
+				entry.Time,
+				entry.LogLevel,
+				entry.Username,
+				entry.FullName,
+				entry.Host,
+				entry.Method,
+				entry.RequestPath,
+				entry.StatusCode
+			});
+			Console.WriteLine(entry);
+			_connection.Close();
+		}
+
+		public IEnumerable<LogEntry> GetLogEntries()
+		{
+			string query = """
+				SELECT *
+				FROM "entries"
+			""";
+
+			_connection.Open();
+			var entries = _connection.Query<LogEntry>(query);
+			_connection.Close();
+			return entries;
+		}
+
+		public IEnumerable<LogEntry> GetLogEntriesBetween(DateTime start, DateTime end)
+		{
+			string query = """
+				SELECT *
+				FROM "entries"
+				WHERE "time" BETWEEN @Start AND @End;
+			""";
+
+			_connection.Open();
+			var entries = _connection.Query<LogEntry>(query, new { Start = start, End = end });
+			_connection.Close();
+			return entries;
+		}
+
+		private void CreateDefaultTables()
+		{
+			string query = """
+				CREATE TABLE IF NOT EXISTS "users" (
+					"username" VARCHAR(255) PRIMARY KEY,
+					"full_name" VARCHAR(255)
+				);
+
+				CREATE TABLE IF NOT EXISTS "entries" (
+					"id" BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+					"time" TIMESTAMP NOT NULL,
+					"log_level" VARCHAR(11) NOT NULL,
+					"username" VARCHAR(255) REFERENCES "users"("username"),
+					"host" VARCHAR(255) NOT NULL,
+					"method" VARCHAR(7) NOT NULL,
+					"request_path" VARCHAR(255) NOT NULL,
+					"status_code" INT NOT NULL
+				);
+			""";
+			_connection.Open();
+			_connection.Query(query);
+			_connection.Close();
+		}
+	}
+}
--- NeuLdapMgnt/Api/Program.cs
+++ NeuLdapMgnt/Api/Program.cs
@@ -14,144 +14,186 @@ using NeuLdapMgnt.Models;
 
 namespace NeuLdapMgnt.Api;
 
-internal static class Program {
-    public static readonly SymmetricSecurityKey SecurityKey = Utils.LoadOrCreateSecurityKey("SECURITY_KEY");
+internal static class Program
+{
+	public static readonly SymmetricSecurityKey SecurityKey = Utils.LoadOrCreateSecurityKey("SECURITY_KEY");
 
-    public static readonly string TokenIssuer = typeof(Program).Assembly.FullName!;
+	public static readonly string TokenIssuer = typeof(Program).Assembly.FullName!;
 
-    public static void Main(string[] args) {
-        LdapService ldapService = LdapService.FromEnvs();
+	public static void Main(string[] args)
+	{
+		LdapService ldapService = LdapService.FromEnvs();
+		PostgresService postgresService = PostgresService.FromEnvs();
 
-        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+		WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-        using RequestLoggerProvider? requestLoggerProvider = Environment.GetEnvironmentVariable("LOGS_DIR") is { Length: > 0 } logsDir ? new(logsDir) : null;
-        ILogger?                     requestLogger         = requestLoggerProvider?.CreateLogger(nameof(RequestLogger));
+		using RequestLoggerProvider? requestLoggerProvider = Environment.GetEnvironmentVariable("LOGS_DIR") is { Length: > 0 } logsDir ? new(logsDir) : null;
+		ILogger? requestLogger = requestLoggerProvider?.CreateLogger(nameof(RequestLogger));
 
-        // Create jwt authentication scheme
-        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options => {
-            options.TokenValidationParameters = new TokenValidationParameters {
-                ValidateAudience = false,
-                ValidateIssuer   = true,
-                ValidateLifetime = true,
+		// Create jwt authentication scheme
+		builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+		{
+			options.TokenValidationParameters = new TokenValidationParameters
+			{
+				ValidateAudience = false,
+				ValidateIssuer = true,
+				ValidateLifetime = true,
 
-                RequireAudience = true,
+				RequireAudience = true,
 
-                IssuerSigningKey = SecurityKey,
-                ValidIssuers     = new[] { TokenIssuer },
-                ValidAlgorithms  = new[] { SecurityAlgorithms.HmacSha512 }
-            };
+				IssuerSigningKey = SecurityKey,
+				ValidIssuers = new[] { TokenIssuer },
+				ValidAlgorithms = new[] { SecurityAlgorithms.HmacSha512 }
+			};
 
-            options.Events = new JwtBearerEvents {
-                // Fail if Authorization header is missing
-                OnMessageReceived = (MessageReceivedContext context) => {
-                    var headerValues = context.Request.Headers.Authorization;
-                    if (headerValues.Count == 0 || !headerValues.ToString().StartsWith("bearer", StringComparison.InvariantCultureIgnoreCase))
-                        context.Fail("missing");
+			options.Events = new JwtBearerEvents
+			{
+				// Fail if Authorization header is missing
+				OnMessageReceived = (MessageReceivedContext context) =>
+				{
+					var headerValues = context.Request.Headers.Authorization;
+					if (headerValues.Count == 0 || !headerValues.ToString().StartsWith("bearer", StringComparison.InvariantCultureIgnoreCase))
+						context.Fail("missing");
 
-                    return Task.CompletedTask;
-                },
+					return Task.CompletedTask;
+				},
 
-                // Return 401 from middleware if authentication fails
-                OnChallenge = async (JwtBearerChallengeContext context) => {
-                    context.HandleResponse();
+				// Return 401 from middleware if authentication fails
+				OnChallenge = async (JwtBearerChallengeContext context) =>
+				{
+					context.HandleResponse();
 
-                    string? error = context.AuthenticateFailure switch {
-                        { Message: "missing" } => "Missing json web token.",
-                        not null               => "Invalid json web token.",
-                        _                      => null
-                    };
+					string? error = context.AuthenticateFailure switch
+					{
+						{ Message: "missing" } => "Missing json web token.",
+						not null => "Invalid json web token.",
+						_ => null
+					};
 
-                    if (error is not null) {
-                        context.Response.StatusCode  = StatusCodes.Status401Unauthorized;
-                        context.Response.ContentType = "text/plain";
-                        await context.Response.WriteAsync(context.ErrorDescription.DefaultIfNullOrEmpty(error));
-                        await context.Response.CompleteAsync();
-                    }
-                },
+					if (error is not null)
+					{
+						context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+						context.Response.ContentType = "text/plain";
+						await context.Response.WriteAsync(context.ErrorDescription.DefaultIfNullOrEmpty(error));
+						await context.Response.CompleteAsync();
+					}
+				},
 
-                // Store the audience of the validated toke it the Audience header
-                OnTokenValidated = (TokenValidatedContext context) => {
-                    JwtSecurityToken token = Authenticator.ReadJwtFromRequestHeader(context.Request);
-                    context.Request.Headers.Append("Audience", token.Audiences.FirstOrDefault());
-                    return Task.CompletedTask;
-                }
-            };
-        });
+				// Store the audience of the validated toke it the Audience header
+				OnTokenValidated = (TokenValidatedContext context) =>
+				{
+					JwtSecurityToken token = Authenticator.ReadJwtFromRequestHeader(context.Request);
+					context.Request.Headers.Append("Audience", token.Audiences.FirstOrDefault());
+					return Task.CompletedTask;
+				}
+			};
+		});
 
-        // Add authorization policy that uses the previously created authentication scheme
-        builder.Services.AddAuthorization(options => {
-            options.AddPolicy(JwtBearerDefaults.AuthenticationScheme + "Policy", policy => {
-                policy.RequireAuthenticatedUser();
-                policy.AuthenticationSchemes.Add(JwtBearerDefaults.AuthenticationScheme);
-            });
-        });
+		// Add authorization policy that uses the previously created authentication scheme
+		builder.Services.AddAuthorization(options =>
+		{
+			options.AddPolicy(JwtBearerDefaults.AuthenticationScheme + "Policy", policy =>
+			{
+				policy.RequireAuthenticatedUser();
+				policy.AuthenticationSchemes.Add(JwtBearerDefaults.AuthenticationScheme);
+			});
+		});
 
-        // Add other services
-        builder.Services.AddCors();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSingleton<LdapService>(_ => ldapService);
-        builder.Services.AddSwaggerWrapperGen("neuldapmgnt", "Neu LDAP Management API", "alpha");
+		// Add other services
+		builder.Services.AddCors();
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSingleton(_ => ldapService);
+		builder.Services.AddSingleton(_ => postgresService);
+		builder.Services.AddSwaggerWrapperGen("neuldapmgnt", "Neu LDAP Management API", "alpha");
 
-        WebApplication app = builder.Build();
-        ldapService.Logger = app.Logger;
+		WebApplication app = builder.Build();
+		ldapService.Logger = app.Logger;
 
-        // Add a middleware that logs each request to the console
-        app.Use(async (HttpContext context, RequestDelegate next) => {
-            DateTime    now = DateTime.Now;
-            HttpRequest req = context.Request;
+		// Add a middleware that logs each request to the console
+		app.Use(async (HttpContext context, RequestDelegate next) =>
+		{
+			DateTime now = DateTime.Now;
+			HttpRequest req = context.Request;
 
-            await next(context);
-            app.Logger.LogInformation($"[{now:yyyy.MM.dd - HH:mm:ss}] {req.Host} → {req.Method} {req.Path} ({context.Response.StatusCode})");
+			await next(context);
+			app.Logger.LogInformation($"[{now:yyyy.MM.dd - HH:mm:ss}] {req.Host} → {req.Method} {req.Path} ({context.Response.StatusCode})");
 
-            req.Headers.TryGetValue("Audience", out var aud);
-            requestLogger?.LogInformation($"{aud.ToString().DefaultIfNullOrEmpty("__NOAUTH__")}\t{req.Host}\t{req.Method}\t{req.Path}\t{context.Response.StatusCode}");
-        });
+			req.Headers.TryGetValue("Audience", out var aud);
+			var user = aud.ToString().DefaultIfNullOrEmpty("__NOAUTH__");
 
-        // Add a middleware for handling LDAP connection binding errors
-        app.Use(async (HttpContext context, RequestDelegate next) => {
-            try {
-                await next(context);
-            }
-            catch (LdapBindingException) {
-                const string message = "Unable to connect to the LDAP server.";
-                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+			requestLogger?.LogInformation($"{user}\t{req.Host}\t{req.Method}\t{req.Path}\t{context.Response.StatusCode}");
+			postgresService.CreateLogEntry(new()
+			{
+				Time = now,
+				LogLevel = LogLevel.Information.ToString(),
+				Username = aud.ToString(),
+				FullName = ldapService.TryGetDisplayNameOfEntity(user, typeof(Teacher)),
+				Host = req.Host.ToString(),
+				Method = req.Method.ToString(),
+				RequestPath = req.Path,
+				StatusCode = context.Response.StatusCode
+			});
+		});
 
-                if (context.Request.Headers.Authorization.ToString().StartsWith("bearer", StringComparison.InvariantCultureIgnoreCase)) {
-                    var result = new RequestResult().SetStatus(StatusCodes.Status503ServiceUnavailable).SetErrors(message).RenewToken(context.Request);
-                    await context.Response.WriteAsJsonAsync(result);
-                }
-                else {
-                    await context.Response.WriteAsync(message);
-                }
+		// Add a middleware for handling LDAP connection binding errors
+		app.Use(async (HttpContext context, RequestDelegate next) =>
+		{
+			try
+			{
+				await next(context);
+			}
+			catch (LdapBindingException)
+			{
+				const string message = "Unable to connect to the LDAP server.";
+				context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
 
-                await context.Response.CompleteAsync();
-            }
-        });
+				if (context.Request.Headers.Authorization.ToString().StartsWith("bearer", StringComparison.InvariantCultureIgnoreCase))
+				{
+					var result = new RequestResult().SetStatus(StatusCodes.Status503ServiceUnavailable).SetErrors(message).RenewToken(context.Request);
+					await context.Response.WriteAsJsonAsync(result);
+				}
+				else
+				{
+					await context.Response.WriteAsync(message);
+				}
 
-        // Set CORS to accept any connection
-        app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+				await context.Response.CompleteAsync();
+				postgresService.CreateLogEntry(new()
+				{
+					Time = DateTime.Now,
+					LogLevel = LogLevel.Critical.ToString(),
+					Host = context.Response.HttpContext.Request.Host.ToString(),
+					Method = context.Response.HttpContext.Request.Method.ToString(),
+					RequestPath = context.Response.HttpContext.Request.Path.ToString(),
+					StatusCode = context.Response.StatusCode
+				});
+			}
+		});
 
-        // Add swagger middlewares when in development mode
-        if (app.Environment.IsDevelopment())
-            app.UseSwaggerWrapper("neuldapmgnt");
+		// Set CORS to accept any connection
+		app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
-        // Map endpoints
-        app.MapAuthEndpoints();
-        app.MapClassEndpoints();
-        app.MapStudentEndpoints();
-        app.MapTeacherEndpoints();
-        app.MapInactiveUserEndpoints();
-        app.MapAdminUserEndpoints();
-        app.MapDbManagementEndpoints();
+		// Add swagger middlewares when in development mode
+		if (app.Environment.IsDevelopment())
+			app.UseSwaggerWrapper("neuldapmgnt");
 
-        // Map testing endpoints when in development mode
-        if (app.Environment.IsDevelopment())
-            app.MapTestingEndpoints();
+		// Map endpoints
+		app.MapAuthEndpoints();
+		app.MapClassEndpoints();
+		app.MapStudentEndpoints();
+		app.MapTeacherEndpoints();
+		app.MapInactiveUserEndpoints();
+		app.MapAdminUserEndpoints();
+		app.MapDbManagementEndpoints();
 
-        // Print the current security key as a base64 string when in development mode
-        if (app.Environment.IsDevelopment())
-            app.Logger.LogCritical($"Key: {Convert.ToBase64String(SecurityKey.Key)}");
+		// Map testing endpoints when in development mode
+		if (app.Environment.IsDevelopment())
+			app.MapTestingEndpoints();
 
-        app.Run();
-    }
+		// Print the current security key as a base64 string when in development mode
+		if (app.Environment.IsDevelopment())
+			app.Logger.LogCritical($"Key: {Convert.ToBase64String(SecurityKey.Key)}");
+
+		app.Run();
+	}
 }
