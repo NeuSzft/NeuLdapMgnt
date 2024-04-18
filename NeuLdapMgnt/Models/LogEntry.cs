7e888c48a08e909403db004fd224f3b2391f2abc
Szidor Patrik <patrikp987@gmail.com>
2024-04-18 12:44:52 +0200

    Created LogEntry.cs

---
+++ NeuLdapMgnt/Models/LogEntry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
+namespace NeuLdapMgnt.Models
+{
+	public class LogEntry
+	{
+		[JsonPropertyName("id")]
+		public long Id { get; set; }
+
+		[JsonPropertyName("time")]
+		public required DateTime Time { get; set; }
+
+		[JsonPropertyName("log_level")]
+		public required string LogLevel { get; set; }
+
+		[JsonPropertyName("username")]
+		public string? Username { get; set; }
+
+		[JsonPropertyName("full_name")]
+		public string? FullName { get; set; }
+
+		[JsonPropertyName("host")]
+		public required string Host { get; set; }
+
+		[JsonPropertyName("method")]
+		public required string Method { get; set; }
+
+		[JsonPropertyName("request_path")]
+		public required string RequestPath { get; set; }
+
+		[JsonPropertyName("status_code")]
+		public required int StatusCode { get; set; }
+
+		public override string ToString()
+		{
+			return $"[{Time:yyyy.MM.dd - HH:mm:ss}] {Host} → {Method} {RequestPath} ({StatusCode})";
+		}
+	}
+}
