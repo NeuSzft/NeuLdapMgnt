0bdaebb4306cc9a64542fc0820e190afd7fa0c93
Törőcsik Ádám <toarexer@gmail.com>
2024-04-18 16:55:41 +0200

    Added GET method to /api/defaultadmin/enabled endpoint

--- NeuLdapMgnt/Api/Endpoints/AdminUserEndpoints.cs
+++ NeuLdapMgnt/Api/Endpoints/AdminUserEndpoints.cs
@@ -7,86 +7,100 @@ using NeuLdapMgnt.Models;
 namespace NeuLdapMgnt.Api.Endpoints;
 
 public static class AdminUserEndpoints {
-    public static void MapAdminUserEndpoints(this WebApplication app) {
-        app.MapGet("/api/admins", (LdapService ldap, HttpRequest request) => {
-               IEnumerable<string> uids = ldap.GetMembersOfGroup("admin");
-               return new RequestResult<IEnumerable<string>>().SetValues(uids).RenewToken(request).ToResult();
-           })
-           .WithOpenApi()
-           .WithTags("Admin Users")
-           .RequireAuthorization()
-           .Produces<RequestResult<string[]>>()
-           .Produces<string>(StatusCodes.Status401Unauthorized, "text/plain")
-           .Produces<RequestResult>(StatusCodes.Status503ServiceUnavailable);
+	public static void MapAdminUserEndpoints(this WebApplication app) {
+		app.MapGet("/api/admins", (LdapService ldap, HttpRequest request) => {
+			   IEnumerable<string> uids = ldap.GetMembersOfGroup("admin");
+			   return new RequestResult<IEnumerable<string>>().SetValues(uids).RenewToken(request).ToResult();
+		   })
+		   .WithOpenApi()
+		   .WithTags("Admin Users")
+		   .RequireAuthorization()
+		   .Produces<RequestResult<string[]>>()
+		   .Produces<string>(StatusCodes.Status401Unauthorized, "text/plain")
+		   .Produces<RequestResult>(StatusCodes.Status503ServiceUnavailable);
 
-        app.MapPost("/api/admins/{id}", (LdapService ldap, HttpRequest request, string id) => {
-               var result = ldap.TryAddEntityToGroup("admin", id);
-               return result.RenewToken(request).ToResult();
-           })
-           .WithOpenApi()
-           .WithTags("Admin Users")
-           .RequireAuthorization()
-           .Produces(StatusCodes.Status200OK)
-           .Produces<RequestResult>(StatusCodes.Status400BadRequest)
-           .Produces<string>(StatusCodes.Status401Unauthorized, "text/plain")
-           .Produces<RequestResult>(StatusCodes.Status409Conflict)
-           .Produces<RequestResult>(StatusCodes.Status503ServiceUnavailable);
+		app.MapPost("/api/admins/{id}", (LdapService ldap, HttpRequest request, string id) => {
+			   var result = ldap.TryAddEntityToGroup("admin", id);
+			   return result.RenewToken(request).ToResult();
+		   })
+		   .WithOpenApi()
+		   .WithTags("Admin Users")
+		   .RequireAuthorization()
+		   .Produces(StatusCodes.Status200OK)
+		   .Produces<RequestResult>(StatusCodes.Status400BadRequest)
+		   .Produces<string>(StatusCodes.Status401Unauthorized, "text/plain")
+		   .Produces<RequestResult>(StatusCodes.Status409Conflict)
+		   .Produces<RequestResult>(StatusCodes.Status503ServiceUnavailable);
 
-        app.MapDelete("/api/admins/{id}", (LdapService ldap, HttpRequest request, string id) => {
-               var result = ldap.TryRemoveEntityFromGroup("admin", id);
-               return result.RenewToken(request).ToResult();
-           })
-           .WithOpenApi()
-           .WithTags("Admin Users")
-           .RequireAuthorization()
-           .Produces(StatusCodes.Status200OK)
-           .Produces<RequestResult>(StatusCodes.Status400BadRequest)
-           .Produces<string>(StatusCodes.Status401Unauthorized, "text/plain")
-           .Produces<RequestResult>(StatusCodes.Status503ServiceUnavailable);
+		app.MapDelete("/api/admins/{id}", (LdapService ldap, HttpRequest request, string id) => {
+			   var result = ldap.TryRemoveEntityFromGroup("admin", id);
+			   return result.RenewToken(request).ToResult();
+		   })
+		   .WithOpenApi()
+		   .WithTags("Admin Users")
+		   .RequireAuthorization()
+		   .Produces(StatusCodes.Status200OK)
+		   .Produces<RequestResult>(StatusCodes.Status400BadRequest)
+		   .Produces<string>(StatusCodes.Status401Unauthorized, "text/plain")
+		   .Produces<RequestResult>(StatusCodes.Status503ServiceUnavailable);
 
-        app.MapPut("/api/defaultadmin/enable", async (LdapService ldap, HttpRequest request) => {
-               using StreamReader reader = new(request.Body);
-               string             value  = await reader.ReadToEndAsync();
+		app.MapGet("/api/defaultadmin/enabled", (LdapService ldap, HttpRequest request) => {
+			   string? value = ldap.GetValue(Authenticator.DefaultAdminEnabledValueName, out var error);
+			   if (error is not null)
+				   return new RequestResult<bool>().SetStatus(StatusCodes.Status400BadRequest).SetErrors(error).RenewToken(request).ToResult();
+			   return new RequestResult<bool>().SetValues(bool.TryParse(value, out var enable) && enable).RenewToken(request).ToResult();
+		   })
+		   .WithOpenApi()
+		   .WithTags("Admin Users")
+		   .RequireAuthorization()
+		   .Produces<RequestResult<bool>>()
+		   .Produces<RequestResult<bool>>(StatusCodes.Status400BadRequest)
+		   .Produces<string>(StatusCodes.Status401Unauthorized, "text/plain")
+		   .Produces<RequestResult>(StatusCodes.Status503ServiceUnavailable);
 
-               if (!bool.TryParse(value, out var enable))
-                   return new RequestResult().SetStatus(StatusCodes.Status400BadRequest).SetErrors($"'{value}' is not a valid boolean.").RenewToken(request).ToResult();
+		app.MapPut("/api/defaultadmin/enabled", async (LdapService ldap, HttpRequest request) => {
+			   using StreamReader reader = new(request.Body);
+			   string             value  = await reader.ReadToEndAsync();
 
-               ldap.SetValue(Authenticator.DefaultAdminEnabledValueName, enable.ToString(), out var error);
-               if (error is not null)
-                   return new RequestResult().SetStatus(StatusCodes.Status503ServiceUnavailable).SetErrors(error).RenewToken(request).ToResult();
+			   if (!bool.TryParse(value, out var enable))
+				   return new RequestResult().SetStatus(StatusCodes.Status400BadRequest).SetErrors($"'{value}' is not a valid boolean.").RenewToken(request).ToResult();
 
-               return new RequestResult().RenewToken(request).ToResult();
-           })
-           .WithOpenApi()
-           .WithTags("Admin Users")
-           .RequireAuthorization()
-           .Accepts<bool>("text/plain")
-           .Produces<RequestResult>()
-           .Produces<RequestResult>(StatusCodes.Status400BadRequest)
-           .Produces<string>(StatusCodes.Status401Unauthorized, "text/plain")
-           .Produces<RequestResult>(StatusCodes.Status503ServiceUnavailable);
+			   ldap.SetValue(Authenticator.DefaultAdminEnabledValueName, enable.ToString(), out var error);
+			   if (error is not null)
+				   return new RequestResult().SetStatus(StatusCodes.Status503ServiceUnavailable).SetErrors(error).RenewToken(request).ToResult();
 
-        app.MapPut("/api/defaultadmin/password", async (LdapService ldap, HttpRequest request) => {
-               using StreamReader reader   = new(request.Body);
-               string             password = await reader.ReadToEndAsync();
+			   return new RequestResult().RenewToken(request).ToResult();
+		   })
+		   .WithOpenApi()
+		   .WithTags("Admin Users")
+		   .RequireAuthorization()
+		   .Accepts<bool>("text/plain")
+		   .Produces<RequestResult>()
+		   .Produces<RequestResult>(StatusCodes.Status400BadRequest)
+		   .Produces<string>(StatusCodes.Status401Unauthorized, "text/plain")
+		   .Produces<RequestResult>(StatusCodes.Status503ServiceUnavailable);
 
-               if (password.Length < 8)
-                   return new RequestResult().SetStatus(StatusCodes.Status400BadRequest).SetErrors("Password must be at least 8 characters long.").RenewToken(request).ToResult();
+		app.MapPut("/api/defaultadmin/password", async (LdapService ldap, HttpRequest request) => {
+			   using StreamReader reader   = new(request.Body);
+			   string             password = await reader.ReadToEndAsync();
 
-               UserPassword userPassword = new(password, 16);
-               ldap.SetValue(Authenticator.DefaultAdminPasswordValueName, userPassword.ToString(), out var error);
-               if (error is not null)
-                   return new RequestResult().SetStatus(StatusCodes.Status503ServiceUnavailable).SetErrors(error).RenewToken(request).ToResult();
+			   if (password.Length < 8)
+				   return new RequestResult().SetStatus(StatusCodes.Status400BadRequest).SetErrors("Password must be at least 8 characters long.").RenewToken(request).ToResult();
 
-               return new RequestResult().RenewToken(request).ToResult();
-           })
-           .WithOpenApi()
-           .WithTags("Admin Users")
-           .RequireAuthorization()
-           .Accepts<string>("text/plain")
-           .Produces<RequestResult>()
-           .Produces<RequestResult>(StatusCodes.Status400BadRequest)
-           .Produces<string>(StatusCodes.Status401Unauthorized, "text/plain")
-           .Produces<RequestResult>(StatusCodes.Status503ServiceUnavailable);
-    }
+			   UserPassword userPassword = new(password, 16);
+			   ldap.SetValue(Authenticator.DefaultAdminPasswordValueName, userPassword.ToString(), out var error);
+			   if (error is not null)
+				   return new RequestResult().SetStatus(StatusCodes.Status503ServiceUnavailable).SetErrors(error).RenewToken(request).ToResult();
+
+			   return new RequestResult().RenewToken(request).ToResult();
+		   })
+		   .WithOpenApi()
+		   .WithTags("Admin Users")
+		   .RequireAuthorization()
+		   .Accepts<string>("text/plain")
+		   .Produces<RequestResult>()
+		   .Produces<RequestResult>(StatusCodes.Status400BadRequest)
+		   .Produces<string>(StatusCodes.Status401Unauthorized, "text/plain")
+		   .Produces<RequestResult>(StatusCodes.Status503ServiceUnavailable);
+	}
 }
