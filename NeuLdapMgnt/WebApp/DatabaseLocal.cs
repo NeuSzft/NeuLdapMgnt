efa97be4cec2ff41bf9be4e5f93bc1b1036adeb6
Szidor Patrik <patrikp987@gmail.com>
2024-04-19 20:15:22 +0200

    Renamed Lists to DatabaseLocal

---
+++ NeuLdapMgnt/WebApp/DatabaseLocal.cs
@@ -0,0 +1,14 @@
+﻿using NeuLdapMgnt.WebApp.Model;
+using NeuLdapMgnt.Models;
+
+namespace NeuLdapMgnt.WebApp
+{
+	public static class DatabaseLocal
+	{
+		public static List<Student> Students { get; set; } = new();
+
+		public static List<Teacher> Teachers { get; set; } = new();
+
+		public static List<string> Admins { get; set; } = new();
+	}
+}
