2280fb7bcd73ede687fd2d383f960db717566bcc
Törőcsik Ádám <toarexer@gmail.com>
2024-04-23 16:27:27 +0200

    Classes can be set to nothing or an empty array now

--- NeuLdapMgnt/Api/LdapServiceExtensions/ValueExtensions.cs
+++ NeuLdapMgnt/Api/LdapServiceExtensions/ValueExtensions.cs
@@ -5,59 +5,67 @@ namespace NeuLdapMgnt.Api.LdapServiceExtensions;
 
 // TODO: Add xml doc comments to this class
 public static class ValueExtensions {
-    public static bool ValueExists(this LdapService ldap, string name) {
-        SearchRequest   request  = new($"cn={name},ou=values,{ldap.DomainComponents}", LdapService.AnyFilter, SearchScope.Base, null);
-        SearchResponse? response = ldap.TryRequest(request) as SearchResponse;
+	public static bool ValueExists(this LdapService ldap, string name) {
+		SearchRequest   request  = new($"cn={name},ou=values,{ldap.DomainComponents}", LdapService.AnyFilter, SearchScope.Base, []);
+		SearchResponse? response = ldap.TryRequest(request) as SearchResponse;
 
-        return response?.Entries.Count == 1;
-    }
+		return response?.Entries.Count == 1;
+	}
 
-    public static string? GetValue(this LdapService ldap, string name, out string? error) {
-        SearchRequest   request  = new($"cn={name},ou=values,{ldap.DomainComponents}", LdapService.AnyFilter, SearchScope.Base, null);
-        SearchResponse? response = ldap.TryRequest(request, out error) as SearchResponse;
+	public static string? GetValue(this LdapService ldap, string name, out string? error) {
+		SearchRequest   request  = new($"cn={name},ou=values,{ldap.DomainComponents}", LdapService.AnyFilter, SearchScope.Base, "description");
+		SearchResponse? response = ldap.TryRequest(request, out error) as SearchResponse;
 
-        if (response is null || response.Entries.Count == 0 || response.Entries[0].Attributes["description"].Count == 0)
-            return null;
+		if (response is null || response.Entries.Count == 0 || response.Entries[0].Attributes["description"].Count == 0)
+			return null;
 
-        return response.Entries[0].Attributes["description"][0].ToString();
-    }
+		return response.Entries[0].Attributes["description"][0].ToString();
+	}
 
-    public static bool SetValue(this LdapService ldap, string name, string value, out string? error) {
-        ldap.TryRequest(new AddRequest($"ou=values,{ldap.DomainComponents}", "organizationalUnit"));
+	public static bool SetValue(this LdapService ldap, string name, string value, out string? error) {
+		ldap.TryRequest(new AddRequest($"ou=values,{ldap.DomainComponents}", "organizationalUnit"), false);
 
-        if (!ldap.ValueExists(name)) {
-            AddRequest addRequest = new($"cn={name},ou=values,{ldap.DomainComponents}",
-                new("objectClass", "applicationProcess"),
-                new("description", value)
-            );
+		if (!ldap.ValueExists(name)) {
+			AddRequest addRequest = new($"cn={name},ou=values,{ldap.DomainComponents}",
+				new("objectClass", "applicationProcess"),
+				new("description", value)
+			);
 
-            return ldap.TryRequest(addRequest, out error) is not null;
-        }
+			return ldap.TryRequest(addRequest, out error) is not null;
+		}
 
-        DirectoryAttributeModification mod = new() {
-            Name      = "description",
-            Operation = DirectoryAttributeOperation.Replace
-        };
-        mod.Add(value);
+		DirectoryAttributeModification mod = new() {
+			Name = "description",
+			Operation = DirectoryAttributeOperation.Replace
+		};
+		mod.Add(value);
 
-        ModifyRequest modRequest = new($"cn={name},ou=values,{ldap.DomainComponents}");
-        modRequest.Modifications.Add(mod);
+		ModifyRequest modRequest = new($"cn={name},ou=values,{ldap.DomainComponents}");
+		modRequest.Modifications.Add(mod);
 
-        return ldap.TryRequest(modRequest, out error) is not null;
-    }
+		return ldap.TryRequest(modRequest, out error) is not null;
+	}
 
-    public static Dictionary<string, string> GetAllValues(this LdapService ldap, out string? error) {
-        SearchRequest   request  = new($"ou=values,{ldap.DomainComponents}", LdapService.AnyFilter, SearchScope.OneLevel, null);
-        SearchResponse? response = ldap.TryRequest(request, out error) as SearchResponse;
+	public static bool UnsetValue(this LdapService ldap, string name, out string? error, bool returnTrueIfNotExists = false) {
+		if (ldap.ValueExists(name))
+			return ldap.TryRequest(new DeleteRequest($"cn={name},ou=values,{ldap.DomainComponents}"), out error) is not null;
 
-        if (response is null)
-            return [];
+		error = null;
+		return returnTrueIfNotExists;
+	}
 
-        Dictionary<string, string> values = new();
+	public static Dictionary<string, string> GetAllValues(this LdapService ldap, out string? error) {
+		SearchRequest   request  = new($"ou=values,{ldap.DomainComponents}", LdapService.AnyFilter, SearchScope.OneLevel, "description");
+		SearchResponse? response = ldap.TryRequest(request, out error) as SearchResponse;
 
-        foreach (SearchResultEntry entry in response.Entries)
-            values.Add(entry.Attributes["cn"].GetValues(typeof(string))[0].ToString()!, entry.Attributes["description"].GetValues(typeof(string))[0].ToString()!);
+		if (response is null)
+			return [];
 
-        return values;
-    }
+		Dictionary<string, string> values = new();
+
+		foreach (SearchResultEntry entry in response.Entries)
+			values.Add(entry.Attributes["cn"].GetValues(typeof(string))[0].ToString()!, entry.Attributes["description"].GetValues(typeof(string))[0].ToString()!);
+
+		return values;
+	}
 }
