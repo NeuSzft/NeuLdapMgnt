115f4c22b0563caba12639d8e82409bc0b5621ec
Szidor Patrik <patrikp987@gmail.com>
2024-04-24 23:21:25 +0200

    Added services and refactored existing components accordingly

--- NeuLdapMgnt/WebApp/Program.cs
+++ NeuLdapMgnt/WebApp/Program.cs
@@ -1,7 +1,9 @@
+using BlazorBootstrap;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NeuLdapMgnt.WebApp;
 using NeuLdapMgnt.WebApp.Requests;
+using NeuLdapMgnt.WebApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,13 +11,23 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazorBootstrap();
 
+ApiRequests apiRequests;
 if (builder.HostEnvironment.IsDevelopment())
 {
-	builder.Services.AddSingleton<ApiRequests>(_ => new("http://localhost:5000"));
+	apiRequests = new("http://localhost:5000");
+	builder.Services.AddSingleton<ApiRequests>(_ => apiRequests);
 }
 else
 {
-	builder.Services.AddSingleton<ApiRequests>(_ => new(builder.HostEnvironment.BaseAddress));
+	apiRequests = new(builder.HostEnvironment.BaseAddress);
+	builder.Services.AddSingleton<ApiRequests>(_ => apiRequests);
 }
+
+ToastService toastService = new();
+ModalService modalService = new();
+
+builder.Services.AddSingleton<ToastService>(_ => toastService);
+builder.Services.AddSingleton<ModalService>(_ => modalService);
+builder.Services.AddSingleton<LocalDbService>(_ => new(apiRequests, modalService, toastService));
 
 await builder.Build().RunAsync();
---
+++ NeuLdapMgnt/WebApp/Services/LocalDbService.cs
@@ -0,0 +1,231 @@
+﻿using NeuLdapMgnt.WebApp.Model;
+using NeuLdapMgnt.Models;
+using BlazorBootstrap;
+using NeuLdapMgnt.WebApp.ComponentOptions;
+using NeuLdapMgnt.WebApp.Components;
+using NeuLdapMgnt.WebApp.Requests;
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics;
+using System.Net;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace NeuLdapMgnt.WebApp.Services
+{
+	public class LocalDbService
+	{
+		[Inject]
+		private ApiRequests ApiRequests { get; set; }
+
+		[Inject]
+		private ModalService ModalService { get; set; }
+
+		[Inject]
+		private ToastService ToastService { get; set; }
+
+		public List<Student> Students { get; set; } = new();
+
+		public List<Teacher> Teachers { get; set; } = new();
+
+		public List<string> Admins { get; set; } = new();
+
+		public List<string> Classes { get; set; } = new();
+
+		public LocalDbService(ApiRequests apiRequests, ModalService modalService, ToastService toastService)
+		{
+			ApiRequests = apiRequests;
+			ModalService = modalService;
+			ToastService = toastService;
+		}
+
+		public async Task FetchClasses()
+		{
+			try
+			{
+				Classes.Clear();
+
+				var response = await ApiRequests.GetClassesAsync();
+				if (response.IsSuccess())
+				{
+					Classes = response.Values.SelectMany(x => x).OrderBy(x => x).ToList();
+				}
+				if (response.Errors.Any())
+				{
+					ToastService.Notify(ToastMessages.Error(response.GetError()));
+				}
+			}
+			catch (Exception e)
+			{
+				string message = e is HttpRequestException re ? re.GetErrorMessage() : e.Message;
+				ToastService.Notify(ToastMessages.Error(message));
+			}
+		}
+
+		public async Task FetchStudents()
+		{
+			try
+			{
+				Students.Clear();
+
+				var response = await ApiRequests.GetStudentsAsync();
+				if (response.IsSuccess())
+				{
+					Students = new(response.Values);
+				}
+
+				if (response.Errors.Any())
+				{
+					ToastService.Notify(ToastMessages.Error(response.GetError()));
+				}
+			}
+			catch (Exception e)
+			{
+				string message = e is HttpRequestException re ? re.GetErrorMessage() : e.Message;
+				ToastService.Notify(ToastMessages.Error(message));
+			}
+		}
+
+		public async Task FetchTeachers()
+		{
+			try
+			{
+				Teachers.Clear();
+
+				var responseTeachers = await ApiRequests.GetTeachersAsync();
+				if (responseTeachers.IsSuccess())
+				{
+					Teachers = new(responseTeachers.Values);
+				}
+
+				if (responseTeachers.Errors.Any())
+				{
+					ToastService.Notify(ToastMessages.Error(responseTeachers.GetError()));
+				}
+			}
+			catch (HttpRequestException httpError)
+			{
+				if (httpError.StatusCode is HttpStatusCode.BadRequest)
+				{
+					ToastService.Notify(ToastMessages.NoConnection());
+				}
+				else
+				{
+					ToastService.Notify(ToastMessages.Error(httpError.Message));
+				}
+			}
+			catch (Exception e)
+			{
+				ToastService.Notify(ToastMessages.Error(e.Message));
+			}
+		}
+
+		public async Task FetchAdmins()
+		{
+			try
+			{
+				Admins.Clear();
+
+				var responseAdmins = await ApiRequests.GetAdminsAsync();
+				if (responseAdmins.IsSuccess())
+				{
+					Admins = new(responseAdmins.Values[0]);
+				}
+
+				if (responseAdmins.Errors.Any())
+				{
+					ToastService.Notify(ToastMessages.Error(responseAdmins.GetError()));
+				}
+			}
+			catch (HttpRequestException httpError)
+			{
+				if (httpError.StatusCode is HttpStatusCode.BadRequest)
+				{
+					ToastService.Notify(ToastMessages.NoConnection());
+				}
+				else
+				{
+					ToastService.Notify(ToastMessages.Error(httpError.Message));
+				}
+			}
+			catch (Exception e)
+			{
+				ToastService.Notify(ToastMessages.Error(e.Message));
+			}
+		}
+
+		public async Task AddStudent(Student student)
+		{
+			try
+			{
+				var response = await ApiRequests.AddStudentAsync(student);
+
+				if (response.IsSuccess())
+				{
+					ToastService.Notify(ToastMessages.Success($"{student.FullName} ({student.Id}) was added!"));
+				}
+
+			}
+			catch (Exception e)
+			{
+				string message = e is HttpRequestException re ? re.GetErrorMessage() : e.Message;
+				ToastService.Notify(ToastMessages.Error(message));
+			}
+		}
+
+		public async Task UpdateStudentClassInBulk(IEnumerable<Student> students, string newClass)
+		{
+			try
+			{
+				foreach (var student in students)
+				{
+					if (student.Class.Equals(newClass))
+					{
+						continue;
+					}
+
+					student.Class = newClass;
+					await ApiRequests.UpdateStudentAsync(student.Id, student);
+				}
+				ToastService.Notify(ToastMessages.Success("Selected students were updated."));
+			}
+			catch (Exception e)
+			{
+				string message = e is HttpRequestException re ? re.GetErrorMessage() : e.Message;
+				ToastService.Notify(ToastMessages.Error(message));
+			}
+		}
+
+		public async Task DeleteStudentsInBulk(IEnumerable<Student> students)
+		{
+			try
+			{
+				foreach (var student in students)
+				{
+					await ApiRequests.DeleteStudentAsync(student.Id);
+				}
+				ToastService.Notify(ToastMessages.Success("Selected students were deleted!"));
+			}
+			catch (Exception e)
+			{
+				string message = e is HttpRequestException re ? re.GetErrorMessage() : e.Message;
+				ToastService.Notify(ToastMessages.Error(message));
+			}
+		}
+
+		public async Task DeleteTeachersInBulk(IEnumerable<Teacher> teachers)
+		{
+			try
+			{
+				foreach (var teacher in teachers)
+				{
+					await ApiRequests.DeleteTeacherAsync(teacher.Id);
+				}
+				ToastService.Notify(ToastMessages.Success("Selected teachers were deleted!"));
+			}
+			catch (Exception e)
+			{
+				string message = e is HttpRequestException re ? re.GetErrorMessage() : e.Message;
+				ToastService.Notify(ToastMessages.Error(message));
+			}
+		}
+	}
+}
