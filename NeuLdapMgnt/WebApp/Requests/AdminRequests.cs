34cd618587ea5e08a1509c78d287189b451ad44e
Szidor Patrik <patrikp987@gmail.com>
2024-04-25 23:38:31 +0200

    Update AdminRequests.cs

--- NeuLdapMgnt/WebApp/Requests/AdminRequests.cs
+++ NeuLdapMgnt/WebApp/Requests/AdminRequests.cs
@@ -6,5 +6,11 @@ namespace NeuLdapMgnt.WebApp.Requests
 	{
 		public static async Task<RequestResult<IEnumerable<string>>> GetAdminsAsync(this ApiRequests apiRequests)
 			=> await apiRequests.SendRequestAsync<IEnumerable<string>>(HttpMethod.Get, "api/admins");
+
+		public static async Task<RequestResult> AddAdminAsync(this ApiRequests apiRequests, string id)
+			=> await apiRequests.SendRequestAsync<RequestResult>(HttpMethod.Post, $"api/admins/{id}");
+
+		public static async Task<RequestResult> DeleteAdminAsync(this ApiRequests apiRequests, string id)
+			=> await apiRequests.SendRequestAsync<RequestResult>(HttpMethod.Delete, $"api/admins/{id}");
 	}
 }
