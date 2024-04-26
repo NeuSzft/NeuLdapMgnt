bdc58d18400bb2f49f256659056b1f9b8ac829c6
Szidor Patrik <patrikp987@gmail.com>
2024-04-26 22:00:46 +0200

    Changed id type in StudentRequests

--- NeuLdapMgnt/WebApp/Requests/StudentRequests.cs
+++ NeuLdapMgnt/WebApp/Requests/StudentRequests.cs
@@ -7,16 +7,16 @@ namespace NeuLdapMgnt.WebApp.Requests
 		public static async Task<RequestResult<Student>> GetStudentsAsync(this ApiRequests apiRequests)
 			=> await apiRequests.SendRequestAsync<Student>(HttpMethod.Get, "api/students");
 
-		public static async Task<RequestResult<Student>> GetStudentAsync(this ApiRequests apiRequests, long id)
+		public static async Task<RequestResult<Student>> GetStudentAsync(this ApiRequests apiRequests, string id)
 			=> await apiRequests.SendRequestAsync<Student>(HttpMethod.Get, $"api/students/{id}");
 
 		public static async Task<RequestResult<Student>> AddStudentAsync(this ApiRequests apiRequests, Student student)
 			=> await apiRequests.SendRequestAsync<Student>(HttpMethod.Post, "api/students", student);
 
-		public static async Task<RequestResult<Student>> UpdateStudentAsync(this ApiRequests apiRequests, long id, Student student)
+		public static async Task<RequestResult<Student>> UpdateStudentAsync(this ApiRequests apiRequests, string id, Student student)
 			=> await apiRequests.SendRequestAsync<Student>(HttpMethod.Put, $"api/students/{id}", student);
 
-		public static async Task<RequestResult<Student>> DeleteStudentAsync(this ApiRequests apiRequests, long id)
+		public static async Task<RequestResult<Student>> DeleteStudentAsync(this ApiRequests apiRequests, string id)
 			=> await apiRequests.SendRequestAsync<Student>(HttpMethod.Delete, $"api/students/{id}");
 	}
 }
