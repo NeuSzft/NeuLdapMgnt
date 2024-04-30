52134ac83fa70e044acfcfb60741d29d83fdb9d0
Törőcsik Ádám <toarexer@gmail.com>
2024-04-30 17:50:06 +0200

    Added xml doc comments to ValueExtensions and LdapDbDump

--- NeuLdapMgnt/Api/LdapServiceExtensions/ValueExtensions.cs
+++ NeuLdapMgnt/Api/LdapServiceExtensions/ValueExtensions.cs
@@ -3,17 +3,25 @@ using System.DirectoryServices.Protocols;
 
 namespace NeuLdapMgnt.Api.LdapServiceExtensions;
 
-// TODO: Add xml doc comments to this class
 public static class ValueExtensions {
-	public static bool ValueExists(this LdapService ldap, string name) {
-		SearchRequest   request  = new($"cn={name},ou=values,{ldap.DomainComponents}", LdapService.AnyFilter, SearchScope.Base, []);
+	/// <summary>Checks if the key-value pair exists within the database.</summary>
+	/// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
+	/// <param name="key">The key or name of the pair.</param>
+	/// <returns><c>true</c> if the key-value pair exists or <c>false</c> if it does not exist or the request fails.</returns>
+	public static bool ValueExists(this LdapService ldap, string key) {
+		SearchRequest   request  = new($"cn={key},ou=values,{ldap.DomainComponents}", LdapService.AnyFilter, SearchScope.Base, []);
 		SearchResponse? response = ldap.TryRequest(request) as SearchResponse;
 
 		return response?.Entries.Count == 1;
 	}
 
-	public static string? GetValue(this LdapService ldap, string name, out string? error) {
-		SearchRequest   request  = new($"cn={name},ou=values,{ldap.DomainComponents}", LdapService.AnyFilter, SearchScope.Base, "description");
+	/// <summary>Tries to get the value of a key-value pair from the database.</summary>
+	/// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
+	/// <param name="key">The key or name of the pair.</param>
+	/// <param name="error">When the method returns, this will contain the error message if there was one. Otherwise it will be set to <c>null</c>.</param>
+	/// <returns>The value the pair or <c>null</c> if it does not exist or the request fails.</returns>
+	public static string? GetValue(this LdapService ldap, string key, out string? error) {
+		SearchRequest   request  = new($"cn={key},ou=values,{ldap.DomainComponents}", LdapService.AnyFilter, SearchScope.Base, "description");
 		SearchResponse? response = ldap.TryRequest(request, out error) as SearchResponse;
 
 		if (response is null || response.Entries.Count == 0 || response.Entries[0].Attributes["description"].Count == 0)
@@ -22,11 +30,17 @@ public static class ValueExtensions {
 		return response.Entries[0].Attributes["description"][0].ToString();
 	}
 
-	public static bool SetValue(this LdapService ldap, string name, string value, out string? error) {
+	/// <summary>Tries to set the value of a key-value pair in the database.</summary>
+	/// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
+	/// <param name="key">The key or name of the pair.</param>
+	/// <param name="value">The new value of the pair.</param>
+	/// <param name="error">When the method returns, this will contain the error message if there was one. Otherwise it will be set to <c>null</c>.</param>
+	/// <returns><c>true</c> if the key-value pair fas successfully set or <c>false</c> if the request fails.</returns>
+	public static bool SetValue(this LdapService ldap, string key, string value, out string? error) {
 		ldap.TryRequest(new AddRequest($"ou=values,{ldap.DomainComponents}", "organizationalUnit"), false);
 
-		if (!ldap.ValueExists(name)) {
-			AddRequest addRequest = new($"cn={name},ou=values,{ldap.DomainComponents}",
+		if (!ldap.ValueExists(key)) {
+			AddRequest addRequest = new($"cn={key},ou=values,{ldap.DomainComponents}",
 				new("objectClass", "applicationProcess"),
 				new("description", value)
 			);
@@ -40,20 +54,30 @@ public static class ValueExtensions {
 		};
 		mod.Add(value);
 
-		ModifyRequest modRequest = new($"cn={name},ou=values,{ldap.DomainComponents}");
+		ModifyRequest modRequest = new($"cn={key},ou=values,{ldap.DomainComponents}");
 		modRequest.Modifications.Add(mod);
 
 		return ldap.TryRequest(modRequest, out error) is not null;
 	}
 
-	public static bool UnsetValue(this LdapService ldap, string name, out string? error, bool successIfNotExists = false) {
-		if (ldap.ValueExists(name))
-			return ldap.TryRequest(new DeleteRequest($"cn={name},ou=values,{ldap.DomainComponents}"), out error) is not null;
+	/// <summary>Tries to unset (delete) the value of a key-value pair in the database.</summary>
+	/// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
+	/// <param name="key">The key or name of the pair.</param>
+	/// <param name="error">When the method returns, this will contain the error message if there was one. Otherwise it will be set to <c>null</c>.</param>
+	/// <param name="successIfNotExists">If <c>true</c> the operation will be treated as a success even if the key-value pair does not exists.</param>
+	/// <returns><c>true</c> if the key-value pair fas successfully unset or <c>false</c> if the request fails.</returns>
+	public static bool UnsetValue(this LdapService ldap, string key, out string? error, bool successIfNotExists = false) {
+		if (ldap.ValueExists(key))
+			return ldap.TryRequest(new DeleteRequest($"cn={key},ou=values,{ldap.DomainComponents}"), out error) is not null;
 
 		error = null;
 		return successIfNotExists;
 	}
 
+	/// <summary>Gets all key-value pair from the database.</summary>
+	/// <param name="ldap">The <see cref="LdapService"/> the method should use.</param>
+	/// <param name="error">When the method returns, this will contain the error message if there was one. Otherwise it will be set to <c>null</c>.</param>
+	/// <returns>A <see cref="Dictionary{TKey,TValue}">Dictionary&lt;string, string&gt;</see> containing the key-value pairs or an empty collection if there was an error.</returns>
 	public static Dictionary<string, string> GetAllValues(this LdapService ldap, out string? error) {
 		SearchRequest   request  = new($"ou=values,{ldap.DomainComponents}", LdapService.AnyFilter, SearchScope.OneLevel, "cn", "description");
 		SearchResponse? response = ldap.TryRequest(request, out error) as SearchResponse;
--- NeuLdapMgnt/Models/LdapDbDump.cs
+++ NeuLdapMgnt/Models/LdapDbDump.cs
@@ -3,20 +3,25 @@ using System.Text.Json.Serialization;
 
 namespace NeuLdapMgnt.Models;
 
-// TODO: Add xml doc comments to this class
+/// <summary>Represents the contents of an LDAP database.</summary>
 public record LdapDbDump {
+    /// <summary>The <see cref="Student"/> entities.</summary>
     [JsonRequired, JsonPropertyName("students")]
     public required IEnumerable<Student> Students { get; init; }
 
+    /// <summary>The <see cref="Teacher"/> entities.</summary>
     [JsonRequired, JsonPropertyName("teachers")]
     public required IEnumerable<Teacher> Teachers { get; init; }
 
+    /// <summary>The uids of the "<c>inactive</c>" group.</summary>
     [JsonRequired, JsonPropertyName("inactives")]
     public required IEnumerable<string> Inactives { get; init; }
 
+    /// <summary>The uids of the "<c>admin</c>" group.</summary>
     [JsonRequired, JsonPropertyName("admins")]
     public required IEnumerable<string> Admins { get; init; }
 
+    /// <summary>A dictionary of the key-value pairs.</summary>
     [JsonRequired, JsonPropertyName("values")]
     public required Dictionary<string, string> Values { get; init; }
 }
