30d9f4aa141834c78ffcdabee6f37810f360ceab
Törőcsik Ádám <toarexer@gmail.com>
2024-05-01 00:15:03 +0200

    Added option to download fetched logs

--- NeuLdapMgnt/WebApp/wwwroot/js/scripts.js
+++ NeuLdapMgnt/WebApp/wwwroot/js/scripts.js
@@ -1,13 +1,14 @@
 // Download json as a file
-self.downloadJsonFile = function (file, json) {
-    const blob = new Blob([new TextEncoder().encode(json)], { type: 'application/json;charset=utf-8' });
-
+self.downloadFile = function (file, type, content) {
+    const blob = new Blob([new TextEncoder().encode(content)], {type: type});
     const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
+    const link = document.createElement("a");
+
     link.href = url;
     link.download = file;
     document.body.appendChild(link);
     link.click();
+
     document.body.removeChild(link);
     URL.revokeObjectURL(url);
 }
