3ed6a7b9a3700f3a0ed30d774657a80cdf29a557
Törőcsik Ádám <toarexer@gmail.com>
2024-06-11 21:23:39 +0200

    Fixed /api/inactives endpoint response

--- NeuLdapMgnt/Api/Endpoints/InactiveUserEndpoints.cs
+++ NeuLdapMgnt/Api/Endpoints/InactiveUserEndpoints.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
@@ -9,7 +10,9 @@ namespace NeuLdapMgnt.Api.Endpoints;
 public static class InactiveUserEndpoints {
 	public static void MapInactiveUserEndpoints(this WebApplication app) {
 		app.MapGet("/api/inactives", (LdapService ldap, HttpRequest request) => {
-				string[] uids = ldap.GetAllEntities<Employee>().Values.Where(x => x.IsInactive).Select(x => x.Id).ToArray();
+				IEnumerable<string> employeeUids = ldap.GetAllEntities<Employee>().Values.Where(x => x.IsInactive).Select(x => x.Id);
+				IEnumerable<string> studentUids  = ldap.GetAllEntities<Student>().Values.Where(x => x.IsInactive).Select(x => x.Id.ToString());
+				string[]            uids         = employeeUids.Concat(studentUids).ToArray();
 				return new RequestResult<string>().SetValues(uids).RenewToken(request).ToResult();
 			})
 			.WithOpenApi()
