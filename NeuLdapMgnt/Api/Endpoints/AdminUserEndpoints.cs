f10e0b201f343378686893f8b7c6daf1a5f46d8d
Törőcsik Ádám <toarexer@gmail.com>
2024-06-24 19:05:39 +0200

    Renamed /api/defaultadmin endpoints to /api/default-admin

--- NeuLdapMgnt/Api/Endpoints/AdminUserEndpoints.cs
+++ NeuLdapMgnt/Api/Endpoints/AdminUserEndpoints.cs
@@ -10,8 +10,8 @@ namespace NeuLdapMgnt.Api.Endpoints;
 public static class AdminUserEndpoints {
 	public static void MapAdminUserEndpoints(this WebApplication app) {
 		app.MapGet("/api/admins", (LdapService ldap, HttpRequest request) => {
-				string[] uids = ldap.GetAllEntities<Employee>().Values.Where(x => x.IsAdmin).Select(x => x.Id).ToArray();
-				return new RequestResult<string>().SetValues(uids).RenewToken(request).ToResult();
+				string[] ids = ldap.GetAllEntities<Employee>().Values.Where(x => x.IsAdmin).Select(x => x.Id).ToArray();
+				return new RequestResult<string>().SetValues(ids).RenewToken(request).ToResult();
 			})
 			.WithOpenApi()
 			.WithTags("Admin Users")
@@ -21,10 +21,10 @@ public static class AdminUserEndpoints {
 			.Produces<string>(StatusCodes.Status401Unauthorized, "text/plain")
 			.Produces<RequestResult>(StatusCodes.Status503ServiceUnavailable);
 
-		app.MapGet("/api/defaultadmin/enabled", (LdapService ldap, HttpRequest request) => {
-				string? value = ldap.GetValue(Authenticator.DefaultAdminEnabledValueName, out var error);
+		app.MapGet("/api/default-admin/enabled", (LdapService ldap, HttpRequest request) => {
+				string? value = ldap.GetValue(Authenticator.DefaultAdminEnabledValueName, out string? error);
 				return error is null
-					? new RequestResult<bool>().SetValues(bool.TryParse(value, out var enable) && enable).RenewToken(request).ToResult()
+					? new RequestResult<bool>().SetValues(bool.TryParse(value, out bool enable) && enable).RenewToken(request).ToResult()
 					: new RequestResult<bool>().SetStatus(StatusCodes.Status400BadRequest).SetErrors(error).RenewToken(request).ToResult();
 			})
 			.WithOpenApi()
@@ -36,14 +36,14 @@ public static class AdminUserEndpoints {
 			.Produces<string>(StatusCodes.Status401Unauthorized, "text/plain")
 			.Produces<RequestResult>(StatusCodes.Status503ServiceUnavailable);
 
-		app.MapPut("/api/defaultadmin/enabled", async (LdapService ldap, HttpRequest request) => {
+		app.MapPut("/api/default-admin/enabled", async (LdapService ldap, HttpRequest request) => {
 				using StreamReader reader = new(request.Body);
 				string             value  = await reader.ReadToEndAsync();
 
-				if (!bool.TryParse(value, out var enable))
+				if (!bool.TryParse(value, out bool enable))
 					return new RequestResult().SetStatus(StatusCodes.Status400BadRequest).SetErrors($"'{value}' is not a valid boolean.").RenewToken(request).ToResult();
 
-				ldap.SetValue(Authenticator.DefaultAdminEnabledValueName, enable.ToString(), out var error);
+				ldap.SetValue(Authenticator.DefaultAdminEnabledValueName, enable.ToString(), out string? error);
 				return error is null
 					? new RequestResult().RenewToken(request).ToResult()
 					: new RequestResult().SetStatus(StatusCodes.Status503ServiceUnavailable).SetErrors(error).RenewToken(request).ToResult();
@@ -58,11 +58,11 @@ public static class AdminUserEndpoints {
 			.Produces<string>(StatusCodes.Status401Unauthorized, "text/plain")
 			.Produces<RequestResult>(StatusCodes.Status503ServiceUnavailable);
 
-		app.MapPut("/api/defaultadmin/password", async (LdapService ldap, HttpRequest request) => {
+		app.MapPut("/api/default-admin/password", async (LdapService ldap, HttpRequest request) => {
 				using StreamReader reader   = new(request.Body);
 				string             password = await reader.ReadToEndAsync();
 
-				ldap.SetValue(Authenticator.DefaultAdminPasswordValueName, Utils.BCryptHashPassword(password), out var error);
+				ldap.SetValue(Authenticator.DefaultAdminPasswordValueName, Utils.BCryptHashPassword(password), out string? error);
 				return error is null
 					? new RequestResult().RenewToken(request).ToResult()
 					: new RequestResult().SetStatus(StatusCodes.Status503ServiceUnavailable).SetErrors(error).RenewToken(request).ToResult();
