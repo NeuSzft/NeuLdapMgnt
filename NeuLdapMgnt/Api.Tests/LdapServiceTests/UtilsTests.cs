4ec264e9d520fd62b853108cc7c824298595715b
Törőcsik Ádám <toarexer@gmail.com>
2024-07-26 17:19:19 +0200

    Added tests for TryGetClientAddress

--- NeuLdapMgnt/Api.Tests/LdapServiceTests/UtilsTests.cs
+++ NeuLdapMgnt/Api.Tests/LdapServiceTests/UtilsTests.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using Microsoft.AspNetCore.Http;
+
 namespace NeuLdapMgnt.Api.Tests.LdapServiceTests;
 
 file class Dummy : Person {
@@ -52,6 +55,28 @@ public class UtilsTests {
 
 		Environment.SetEnvironmentVariable(env, value);
 		Assert.AreEqual(Utils.IsEnvTrue(env), expected);
+	}
+
+	[TestMethod]
+	[DataRow("1.0.0.1", null, "1.0.0.1")]
+	[DataRow("1.0.0.1", "2.0.0.1", "1.0.0.1")]
+	[DataRow(null, "2.0.0.1", "2.0.0.1")]
+	[DataRow(null, "2.0.0.1, 3.0.0.1, 4.0.0.1", "4.0.0.1")]
+	[DataRow(null, null, "127.0.0.1")]
+	public void TestTryGetClientAddress(string? realIp, string? forwardedFor, string expected) {
+		DefaultHttpContext context = new() {
+			Connection = {
+				RemoteIpAddress = new([ 127, 0, 0, 1 ])
+			},
+			Request = {
+				Headers = {
+					["X-Real-IP"]       = realIp,
+					["X-Forwarded-For"] = forwardedFor
+				}
+			}
+		};
+
+		Assert.AreEqual(expected, context.TryGetClientAddress(true));
 	}
 
 	[TestMethod]
